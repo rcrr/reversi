8de4feadfa344246c8c8e7e114c51aa7005a0427
Roberto Corradini <rob_corradini@yahoo.it>
2010-12-26 08:14:29 +0100

    Checkstyle changest to Reversi and Strategy.

--- java/src/rcrr/reversi/Reversi.java
+++ java/src/rcrr/reversi/Reversi.java
@@ -30,10 +30,6 @@
 
 package rcrr.reversi;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
 import java.io.PrintStream;
 
 import org.joda.time.Period;
@@ -45,11 +41,29 @@ import org.joda.time.Duration;
  * Documentation has to be completed.
  * Testing has to be completed.
  */
-public class Reversi {
+public final class Reversi {
+
+    /** Error code 1. */
+    private static final int ERROR_CODE_1 = 1;
+
+    /** Error code 2. */
+    private static final int ERROR_CODE_2 = 2;
+
+    /** Error code 3. */
+    private static final int ERROR_CODE_3 = 3;
+
+    /** Error code 4. */
+    private static final int ERROR_CODE_4 = 4;
+
+    /** Error code 5. */
+    private static final int ERROR_CODE_5 = 5;
+
+    /** Default game duration in minutes. */
+    private static final int DEFAULT_GAME_DURATION_IN_MINUTES = 30;
 
     /** Class constructor. Not used so far. */
     private Reversi() {
-	throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException();
     }
 
     /**
@@ -59,21 +73,24 @@ public class Reversi {
      * Testing has to be completed.
      * It has to be fully refactored.
      *
-     * @param blStrategy ...
-     * @param whStrategy
-     * @param ps
-     * @param gameDuration
-     * 
+     * @param blStrategy   the black's strategy
+     * @param whStrategy   the white's strategy
+     * @param ps           the output print stream
+     * @param gameDuration the game duration assigned to both players
+     *
      * @return           the game score
      */
-    public static int reversi(Strategy blStrategy, Strategy whStrategy, PrintStream ps, Duration gameDuration) {
+    public static int reversi(final Strategy blStrategy,
+                              final Strategy whStrategy,
+                              final PrintStream ps,
+                              final Duration gameDuration) {
 
-	/** Must be revised!!!! */
-	Actor black = Actor.valueOf("Black Actor", blStrategy);
-	Actor white = Actor.valueOf("White Actor", whStrategy);
-	Game game = Game.initialGame(black, white, gameDuration, ps);
-	game.play();
-	return game.countDiscDifference();
+        /** Must be revised!!!! */
+        final Actor black = Actor.valueOf("Black Actor", blStrategy);
+        final Actor white = Actor.valueOf("White Actor", whStrategy);
+        final Game game = Game.initialGame(black, white, gameDuration, ps);
+        game.play();
+        return game.countDiscDifference();
     }
 
     /**
@@ -81,47 +98,50 @@ public class Reversi {
      *
      * @param args an array having two elements: [black's strategy, white's strategy]
      */
-    public static void main(String[] args) {
-	if (args == null || args.length != 2) {
-	    System.out.println("Argument list error: blackStrategy and whiteStrategy must be provided.");
-	    usage();
-	    System.exit(1);
-	}
-	Strategy s[] = new Strategy[]{null, null};
-	for (int i=0; i<2; i++) {
-	    Object o = null;
-	    try {
-		Class<?> c = Class.forName(args[i]);
-		o = c.newInstance();
-	    } catch (ClassNotFoundException e) {
-		System.out.println("Exception e: " + e);
-		usage();
-		System.exit(2);
-	    } catch (InstantiationException e) {
-		System.out.println("Exception e: " + e);
-		usage();
-		System.exit(3);
-	    } catch (IllegalAccessException e) {
-		System.out.println("Exception e: " + e);
-		usage();
-		System.exit(4);
-	    }
-	    try {
-		s[i] = (Strategy) o;
-	    } catch (ClassCastException e) {
-		System.out.println("Exception e: " + e);
-		usage();
-		System.exit(5);
-	    }
-	}
-	reversi(s[0], s[1], System.out, Period.minutes(30).toStandardDuration());
+    public static void main(final String[] args) {
+        if (args == null || args.length != 2) {
+            System.out.println("Argument list error: blackStrategy and whiteStrategy must be provided.");
+            usage();
+            System.exit(ERROR_CODE_1);
+        }
+        Strategy[] s = new Strategy[]{null, null};
+        for (int i = 0; i < s.length; i++) {
+            Object o = null;
+            try {
+                Class<?> c = Class.forName(args[i]);
+                o = c.newInstance();
+            } catch (ClassNotFoundException e) {
+                System.out.println("Exception e: " + e);
+                usage();
+                System.exit(ERROR_CODE_2);
+            } catch (InstantiationException e) {
+                System.out.println("Exception e: " + e);
+                usage();
+                System.exit(ERROR_CODE_3);
+            } catch (IllegalAccessException e) {
+                System.out.println("Exception e: " + e);
+                usage();
+                System.exit(ERROR_CODE_4);
+            }
+            try {
+                s[i] = (Strategy) o;
+            } catch (ClassCastException e) {
+                System.out.println("Exception e: " + e);
+                usage();
+                System.exit(ERROR_CODE_5);
+            }
+        }
+        reversi(s[0], s[1], System.out,
+                Period.minutes(DEFAULT_GAME_DURATION_IN_MINUTES).toStandardDuration());
     }
 
-    /** returns the usage message */
+    /**
+     * Print the usage message.
+     */
     private static void usage() {
-	System.out.println("usage: java rcrr.reversi.Reversi blackStrategy whiteStrategy");
-	System.out.println("\t Where blackStrategy and whiteStrategy are two classes");
-	System.out.println("\t that implements the rcrr.reversi.Strategy interface.");
+        System.out.println("usage: java rcrr.reversi.Reversi blackStrategy whiteStrategy");
+        System.out.println("\t Where blackStrategy and whiteStrategy are two classes");
+        System.out.println("\t that implements the rcrr.reversi.Strategy interface.");
     }
 
 }
