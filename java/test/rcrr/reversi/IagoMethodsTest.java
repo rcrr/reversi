bee4dea8da18e292fc1954210bfd3e391ada5b2b
Roberto Corradini <rob_corradini@yahoo.it>
2011-07-02 19:50:15 +0200

    Started to implement the 18.12 PAIP paragraph.

---
+++ java/test/rcrr/reversi/IagoMethodsTest.java
@@ -0,0 +1,65 @@
+/*
+ *  IagoMethodsTest.java
+ *
+ *  Copyright (c) 2010, 2011 Roberto Corradini. All rights reserved.
+ *
+ *  This file is part of the reversi program
+ *  http://github.com/rcrr/reversi
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation; either version 3, or (at your option) any
+ *  later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ *  or visit the site <http://www.gnu.org/licenses/>.
+ */
+
+package rcrr.reversi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * Test Suite for the {@code Iago} class.
+ *
+ * @see Iago
+ */
+public class IagoMethodsTest {
+
+    /** Class constructor. */
+    public IagoMethodsTest() { }
+
+    /**
+     * Tests the mobility method.
+     *
+     * @see Iago#mobility(GamePosition)
+     */
+    @Test
+    public final void testMobility() {
+        assertThat("new Iago().mobility(GamePositionFixtures.INITIAL).current() is 4.",
+                   new Iago().mobility(GamePositionFixtures.INITIAL).current(),
+                   is(4));
+        assertThat("new Iago().mobility(GamePositionFixtures.INITIAL).potential() is 6.",
+                   new Iago().mobility(GamePositionFixtures.INITIAL).potential(),
+                   is(6));
+        assertThat("new Iago().mobility(GamePositionFixtures.BLACK_HAS_TO_PASS).current() is 0.",
+                   new Iago().mobility(GamePositionFixtures.BLACK_HAS_TO_PASS).current(),
+                   is(0));
+        assertThat("new Iago().mobility(GamePositionFixtures.BLACK_HAS_TO_PASS).potential() is 5.",
+                   new Iago().mobility(GamePositionFixtures.BLACK_HAS_TO_PASS).potential(),
+                   is(5));
+    }
+
+
+}
