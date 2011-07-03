706dcd18f68d0d83d46351509160ff5a12e1c49f
Roberto Corradini <rob_corradini@yahoo.it>
2011-07-03 20:11:48 +0200

    Inserted edgeIndex method.

--- java/test/rcrr/reversi/IagoMethodsTest.java
+++ java/test/rcrr/reversi/IagoMethodsTest.java
@@ -41,7 +41,7 @@ public class IagoMethodsTest {
     public IagoMethodsTest() { }
 
     /**
-     * Tests the mobility method.
+     * Tests the {@code mobility(GamePosition)} method.
      *
      * @see Iago#mobility(GamePosition)
      */
@@ -61,5 +61,60 @@ public class IagoMethodsTest {
                    is(5));
     }
 
+    /**
+     * Tests the {@code edgeIndex(Player, Board, List)} method.
+     *
+     * @see Iago#edgeIndex(Player, Board, List)
+     */
+    @Test
+    public final void testEdgeIndex() {
+        assertThat("Iago.edgeIndex(Player.BLACK, BoardFixtures.INITIAL, Iago.TOP_EDGE) is 0.",
+                   Iago.edgeIndex(Player.BLACK, BoardFixtures.INITIAL, Iago.TOP_EDGE),
+                   is(0));
+        assertThat("Iago.edgeIndex(Player.WHITE, BoardFixtures.INITIAL, Iago.TOP_EDGE) is 0.",
+                   Iago.edgeIndex(Player.WHITE, BoardFixtures.INITIAL, Iago.TOP_EDGE),
+                   is(0));
+
+        final Board fullEdge = new Board.Builder().withSquaresLiteral(1, 1, 1, 1, 1, 1, 1, 1,
+                                                                      0, 1, 0, 0, 0, 0, 1, 0,
+                                                                      0, 0, 0, 0, 0, 0, 0, 0,
+                                                                      0, 0, 0, 0, 0, 0, 0, 0,
+                                                                      0, 0, 0, 0, 0, 0, 0, 0,
+                                                                      0, 0, 0, 0, 0, 0, 0, 0,
+                                                                      0, 0, 0, 0, 0, 0, 0, 0,
+                                                                      0, 0, 0, 0, 0, 0, 0, 0).build();
+
+        assertThat("When all the squares in the edge are occupied by the PLAYER the value must be 29524.",
+                   Iago.edgeIndex(Player.BLACK, fullEdge, Iago.TOP_EDGE),
+                   is(29524));
+        assertThat("When all the squares in the edge are occupied by the OPPONENT the value must be 59048.",
+                   Iago.edgeIndex(Player.WHITE, fullEdge, Iago.TOP_EDGE),
+                   is(59048));
+
+        assertThat("Iago.edgeIndex(Player.BLACK, BoardFixtures.BLACK_HAS_TO_PASS, Iago.TOP_EDGE) is 35290.",
+                   Iago.edgeIndex(Player.BLACK, BoardFixtures.BLACK_HAS_TO_PASS, Iago.TOP_EDGE),
+                   is(35290));
+        assertThat("Iago.edgeIndex(Player.WHITE, BoardFixtures.BLACK_HAS_TO_PASS, Iago.TOP_EDGE) is 50816.",
+                   Iago.edgeIndex(Player.WHITE, BoardFixtures.BLACK_HAS_TO_PASS, Iago.TOP_EDGE),
+                   is(50816));
+        assertThat("Iago.edgeIndex(Player.BLACK, BoardFixtures.BLACK_HAS_TO_PASS, Iago.BOTTOM_EDGE) is 39355.",
+                   Iago.edgeIndex(Player.BLACK, BoardFixtures.BLACK_HAS_TO_PASS, Iago.BOTTOM_EDGE),
+                   is(39355));
+        assertThat("Iago.edgeIndex(Player.WHITE, BoardFixtures.BLACK_HAS_TO_PASS, Iago.BOTTOM_EDGE) is 49217.",
+                   Iago.edgeIndex(Player.WHITE, BoardFixtures.BLACK_HAS_TO_PASS, Iago.BOTTOM_EDGE),
+                   is(49217));
+        assertThat("Iago.edgeIndex(Player.BLACK, BoardFixtures.BLACK_HAS_TO_PASS, Iago.LEFT_EDGE) is 34999.",
+                   Iago.edgeIndex(Player.BLACK, BoardFixtures.BLACK_HAS_TO_PASS, Iago.LEFT_EDGE),
+                   is(34999));
+        assertThat("Iago.edgeIndex(Player.WHITE, BoardFixtures.BLACK_HAS_TO_PASS, Iago.LEFT_EDGE) is 50306.",
+                   Iago.edgeIndex(Player.WHITE, BoardFixtures.BLACK_HAS_TO_PASS, Iago.LEFT_EDGE),
+                   is(50306));
+        assertThat("Iago.edgeIndex(Player.BLACK, BoardFixtures.BLACK_HAS_TO_PASS, Iago.RIGHT_EDGE) is 26224.",
+                   Iago.edgeIndex(Player.BLACK, BoardFixtures.BLACK_HAS_TO_PASS, Iago.RIGHT_EDGE),
+                   is(26224));
+        assertThat("Iago.edgeIndex(Player.WHITE, BoardFixtures.BLACK_HAS_TO_PASS, Iago.RIGHT_EDGE) is 42638.",
+                   Iago.edgeIndex(Player.WHITE, BoardFixtures.BLACK_HAS_TO_PASS, Iago.RIGHT_EDGE),
+                   is(42638));
+    }
 
 }
