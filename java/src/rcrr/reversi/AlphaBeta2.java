84b7f77d99025bf379ea7e8c9831febee3a38faa
Roberto Corradini <rob_corradini@yahoo.it>
2011-11-13 19:49:22 +0100

    Checkstyle corrections.

--- java/src/rcrr/reversi/AlphaBeta2.java
+++ java/src/rcrr/reversi/AlphaBeta2.java
@@ -24,8 +24,6 @@
 
 package rcrr.reversi;
 
-import rcrr.reversi.Square;
-
 import java.util.Comparator;
 import java.util.Collections;
 import java.util.Map;
@@ -34,9 +32,6 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.HashSet;
 
 /**
  * The {@code AlphaBeta2} class implements {@code DecisionRule} and provides
