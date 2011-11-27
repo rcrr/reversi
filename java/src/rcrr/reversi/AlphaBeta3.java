fa24f78798c82184ec37c7fd1389fbc9a70892bd
Roberto Corradini <rob_corradini@yahoo.it>
2011-11-27 19:22:55 +0100

    A few refactorings into AlphaBeta3.java.

--- java/src/rcrr/reversi/AlphaBeta3.java
+++ java/src/rcrr/reversi/AlphaBeta3.java
@@ -48,29 +48,43 @@ public final class AlphaBeta3 extends AbstractDecisionRule {
             this.value = value;
             this.position = position;
         }
-        final GamePosition position() { return this.position; }
-        final int value() { return this.value; }
+        GamePosition position() { return this.position; }
+        int value() { return this.value; }
     }
 
-    private static final class ValueComparator<Square> implements Comparator<Square> {
+    private static final class ValueComparator implements Comparator<Square> {
+
         private final Map<Square, MoveData> values;
+
         ValueComparator(final Map<Square, MoveData> values) {
             this.values = values;
         }
-        @SuppressWarnings(value = "unchecked")
-        public int compare(final Object object1,
-                           final Object object2) {
-            if (object1 == object2) { return 0; }
-            rcrr.reversi.Square sq0 = (rcrr.reversi.Square) object1;
-            rcrr.reversi.Square sq1 = (rcrr.reversi.Square) object2;
+
+        /**
+         * Compares its two arguments for order.
+         * Returns a negative integer, zero, or a positive integer as the first argument
+         * is less than, equal to, or greater than the second.
+         * <p>
+         * The order is given by the value of the new game position associated to the given square.
+         * Higher value comes first. Two squares having the same value are then sorted by their
+         * natural order.
+         * <p>
+         * Parameters {@code sq0}, and {@code sq1} must be not null.
+         *
+         * @param sq0 the first square object to be compared
+         * @param sq1 the second square object to be compared
+         * @return    a negative integer, zero, or a positive integer as the first argument
+         *            is less than, equal to, or greater than the second
+         */
+        public int compare(final Square sq0,
+                           final Square sq1) {
+            assert (sq0 != null) : "Parameter sq0 cannot be null.";
+            assert (sq1 != null) : "Parameter sq1 cannot be null.";
             final int v0 = values.get(sq0).value();
             final int v1 = values.get(sq1).value();
             if (v0 == v1) {
-                final int ordinalSq0 = sq0.ordinal();
-                final int ordinalSq1 = sq1.ordinal();
-                return (ordinalSq0 > ordinalSq1) ? -1: +1;
-            }
-            if (v0 > v1) {
+                return sq0.compareTo(sq1);
+            } else if (v0 > v1) {
                 return -1;
             } else {
                 return +1;
@@ -78,15 +92,17 @@ public final class AlphaBeta3 extends AbstractDecisionRule {
         }
     }
 
-    private final SortedMap<Square, MoveData> dynamicSortedLegalMoves(final Player player, final Board board, final EvalFunction ef) {
+    private SortedMap<Square, MoveData> dynamicSortedLegalMoves(final Player player,
+                                                                final Board board,
+                                                                final EvalFunction ef) {
         final List<Square> moves = board.legalMoves(player);
         final Map<Square, MoveData> values = new HashMap<Square, MoveData>();
         for (Square move : moves) {
             final GamePosition position = GamePosition.valueOf(board.makeMove(move, player), player.opponent());
-            final MoveData data = new MoveData(position, - ef.eval(position));
+            final MoveData data = new MoveData(position, -ef.eval(position));
             values.put(move, data);
         }
-        final SortedMap<Square, MoveData> results = new TreeMap<Square, MoveData>(new ValueComparator<Square>(values));
+        final SortedMap<Square, MoveData> results = new TreeMap<Square, MoveData>(new ValueComparator(values));
         results.putAll(values);
         return results;
     }
@@ -117,18 +133,17 @@ public final class AlphaBeta3 extends AbstractDecisionRule {
         return multiLevelOrderingSearch(position, LOSING_VALUE, WINNING_VALUE, ply, ef, 0);
     }
 
-
-
     /**
      * Implemented by means of the alpha-beta algorithm applying a dynamic ordering
      * of the moves.
      *
-     * @param position   the game position
-     * @param achievable the upper bound
-     * @param cutoff     the lower bound
-     * @param ply        the search depth
-     * @param ef         the evaluation function
-     * @return a new search node
+     * @param position    the game position
+     * @param achievable  the upper bound
+     * @param cutoff      the lower bound
+     * @param ply         the search depth
+     * @param ef          the evaluation function
+     * @param efNodeValue the node value as returned by the evaluation function
+     * @return            a new search node
      */
     private SearchNode multiLevelOrderingSearch(final GamePosition position,
                                                 final int achievable,
@@ -158,7 +173,7 @@ public final class AlphaBeta3 extends AbstractDecisionRule {
                                                        -cutoff, -node.value(),
                                                        ply - 1,
                                                        ef,
-                                                       - moves.get(move).value())
+                                                       -moves.get(move).value())
                         .negated().value();
                     if (val > node.value()) {
                         node = SearchNode.valueOf(move, val);
