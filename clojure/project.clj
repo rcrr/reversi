42c14cf098ecead65aee75551d6e03b0c690f0ee
Roberto Corradini <rob_corradini@yahoo.it>
2012-02-07 01:29:06 +0100

    Reflection worning removed.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject rcrr.clojure "1.0.0-SNAPSHOT"
+(defproject rcrr.reversi "1.0.0-SNAPSHOT"
             :description "Reversi board program written in clojure"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.clojure/math.numeric-tower "0.0.1"]
