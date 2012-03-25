9d02fb93b85a30d8f691553c9569d4177dd56225
Roberto Corradini <rob_corradini@yahoo.it>
2012-03-25 17:00:44 +0200

    Partial commit. Code compile and pass the tests.

--- java/src/rcrr/reversi/BitBoard.java
+++ java/src/rcrr/reversi/BitBoard.java
@@ -24,6 +24,50 @@
 
 package rcrr.reversi;
 
+//public final class BitBoard extends AbstractBoard {
 public final class BitBoard {
+
+    private static final int BITBOARD_BLACK_INDEX = 0;
+    private static final int BITBOARD_WHITE_INDEX = 1;
+
+    /**
+     * The bitboard field.
+     */
+    private final long[] bitboard;
+
+    /**
+     * Class constructor.
+     * <p>
+     * {@code bitboard} must be not null, and must have a size equal to
+     * two. Overlapping bit set to one are not allowed.
+     *
+     * @param  bitboard the bitboard field
+     */
+    private BitBoard(final long[] bitboard) {
+        assert (bitboard != null) : "Parameter bitboard cannot be null.";
+        assert (bitboard.length == 2) : "Parameter bitboard must have a lenght equal to two.";
+        assert ((bitboard[0] & bitboard[1]) == 0L) : "Parameter bitboard cannot have black and white discs overlapping.";
+        this.bitboard = bitboard.clone();
+    }
+
+    /**
+     * Returns the disk count for the color.
+     *
+     * @param color the color for which the disk count is computed
+     * @return the disk count
+     * @throws NullPointerException if parameter {@code color} is null
+     */
+    public int countPieces(final SquareState color) {
+        if (color == null) {
+            throw new NullPointerException("parameter color must be not null.");
+        }
+        switch (color) {
+        case BLACK: return Long.bitCount(bitboard[BITBOARD_BLACK_INDEX]);
+        case WHITE: return Long.bitCount(bitboard[BITBOARD_WHITE_INDEX]);
+        case EMPTY: return 64 - Long.bitCount(bitboard[BITBOARD_BLACK_INDEX] | bitboard[BITBOARD_WHITE_INDEX]);
+        case OUTER: return 0;
+        default: throw new IllegalArgumentException("Undefined value for color parameter. color=" + color);
+        }
+    }
 
 }
