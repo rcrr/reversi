3de230f55b1180467349736dcee401c56d4dfa11
Roberto Corradini <rob_corradini@yahoo.it>
2012-04-01 21:24:42 +0200

    Refactoring is underway. Code compile. Tests are green.

--- java/src/rcrr/reversi/BoardFactoryHolder.java
+++ java/src/rcrr/reversi/BoardFactoryHolder.java
@@ -29,9 +29,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 
-import java.lang.reflect.Type;
-import java.lang.reflect.WildcardType;
-import java.lang.reflect.ParameterizedType;
+import rcrr.reversi.util.InterfaceCheck;
 
 public final class BoardFactoryHolder {
 
@@ -85,7 +83,7 @@ public final class BoardFactoryHolder {
             }
         }
         System.out.println("Board FactoryClass = " + boardFactoryClass.getName());
-        if (!isBoardFactory(boardFactoryClass)) {
+        if (!InterfaceCheck.isBoardFactory(boardFactoryClass)) {
             throw new RuntimeException(boardFactoryClass.getName() + " does not implement the Board interface. Critical Error!");
         }
         try {
@@ -99,59 +97,6 @@ public final class BoardFactoryHolder {
 
     public final BoardFactory boardFactory() {
         return this.boardFactory;
-    }
-
-    // The three methods has to be put in a dedicated class.
-    // The code is an Utility that has to work nt just for Board
-    // Some refctoring is needed.
-    /**
-     * Returns true if parameter {@code type} is implementing the {@code BoardFactory} interface, false  otherwise.
-     *
-     * @param type the type to check.
-     * @return {@code true} if {@code type} is implementing {@code BoardFactory}, {@code false} otherwise.
-     */
-    private static boolean isBoardFactory(final Type type) {
-        if (type instanceof Class && isBoardFactoryClass((Class) type)) {
-            return true;
-        }
-        if (type instanceof ParameterizedType) {
-            return isBoardFactory(((ParameterizedType) type).getRawType());
-        }
-        if (type instanceof WildcardType) {
-            final Type[] upperBounds = ((WildcardType) type).getUpperBounds();
-            return upperBounds.length != 0 && isBoardFactory(upperBounds[0]);
-        }
-        return false;
-    }
-
-    /**
-     * Checks whether the specified class parameter is an instance of a collection class.
-     *
-     * @param clazz {@code Class} to check.
-     * @return {@code true} is {@code clazz} is instance of a collection class, {@code false} otherwise.
-     */
-    private static boolean isBoardFactoryClass(Class<?> clazz) {
-        List<Class<?>> classes = new ArrayList<Class<?>>();
-        computeClassHierarchy(clazz, classes);
-        return classes.contains(BoardFactory.class);
-    }
-
-    /**
-     * Get all superclasses and interfaces recursively.
-     *
-     * @param clazz The class to start the search with.
-     * @param classes List of classes to which to add all found super classes and interfaces.
-     */
-    private static void computeClassHierarchy(Class<?> clazz, List<Class<?>> classes) {
-        for (Class current = clazz; current != null; current = current.getSuperclass()) {
-            if (classes.contains(current)) {
-                return;
-            }
-            classes.add(current);
-            for (Class currentInterface : current.getInterfaces()) {
-                computeClassHierarchy(currentInterface, classes);
-            }
-        }
     }
 
 }
