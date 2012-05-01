7849841fae6617ad2363c5dea175c95af31f48ca
Roberto Corradini <rob_corradini@yahoo.it>
2012-05-01 21:45:00 +0200

    Documentation refactoring, and style refinements.

--- java/src/rcrr/reversi/AbstractBoard.java
+++ java/src/rcrr/reversi/AbstractBoard.java
@@ -33,11 +33,6 @@ import java.io.Serializable;
 
 public abstract class AbstractBoard implements Board {
 
-    public abstract SquareState get(Square square);
-    public abstract Board makeMove(Square move, Player player);
-    public abstract boolean isLegal(Square move, Player player);
-    public abstract int countPieces(SquareState color);
-
     /**
      * An instance of this class is used as the proxy that enable the board serialization.
      */
@@ -81,34 +76,6 @@ public abstract class AbstractBoard implements Board {
 
     }
 
-    /**
-     * The {@code writeReplace()} method for the serialization proxy pattern.
-     * <p>
-     * The method return a newly created instance of the class {@code EnumMapBoard.SerializationProxy}.
-     * This instance is then serialized instead of the actual board object.
-     * <p>
-     * See the book: <i>"Bloch, Joshua. Effective Java Second Edition. Addison-Wesley, 2008"</i>.
-     *
-     * @return a new seialization proxy for the board object
-     */
-    Object writeReplace() {
-        return new SerializationProxy(this);
-    }
-
-    /**
-     * The {@code readObject()} method for the serialization proxy pattern.
-     * <p>
-     * The method cannot be invoked. It always throws a new exception.
-     * <p>
-     * See the book: <i>"Bloch, Joshua. Effective Java Second Edition. Addison-Wesley, 2008"</i>.
-     *
-     * @param stream the object input stream
-     * @throws InvalidObjectException always
-     */
-    private void readObject(final ObjectInputStream stream) throws InvalidObjectException {
-        throw new InvalidObjectException("Proxy required");
-    }
-
     /** Prime number 17. */
     private static final int PRIME_NUMBER_17 = 17;
 
@@ -133,22 +100,23 @@ public abstract class AbstractBoard implements Board {
     }
 
     /**
-     * Returns a list holding the legal moves that the {@code player} can
-     * do at the board position. When no moves are available to the player
-     * the method returns an empty list.
+     * Returns the disk count for the color.
      *
-     * @param player the player
-     * @return       the moves available to the player
-     * @throws NullPointerException if parameter {@code player} is null
+     * @param color the color for which the disk count is computed
+     * @return      the disk count
+     * @throws NullPointerException if parameter {@code color} is null
      */
-    public List<Square> legalMoves(final Player player) {
-        if (player == null) { throw new NullPointerException("parameter player must be not null."); }
-        final List<Square> legalMoves = new ArrayList<Square>();
-        for (final Square move : Square.values()) {
-            if (isLegal(move, player)) { legalMoves.add(move); }
-        }
-        return legalMoves;
-    }
+    public abstract int countPieces(SquareState color);
+
+    /**
+     * Returns the {@code SquareState} value for the given board's square.
+     * <p>
+     * When {@code square} is {@code null} the method has to return {@code SquareState.OUTER} value.
+     *
+     * @param  square the board square to retrieve the state value
+     * @return        the square state
+     */
+    public abstract SquareState get(Square square);
 
     /**
      * Returns if the player has any legal move given the board state.
@@ -186,6 +154,57 @@ public abstract class AbstractBoard implements Board {
         }
         return hasAnyPlayerAnyLegalMove;
     }
+
+    /**
+     * Returns the boolean value telling if the move, done by the specified player, is legal.
+     * <p>
+     * Parameter {@code move} must be not {@code null}.
+     * Parameter {@code player} must be not {@code null}.
+     *
+     * @param move   the square where to put the new disk
+     * @param player the player moving
+     * @return       true if the move is legal, otherwise false
+     * @throws NullPointerException if parameter {@code move} or {@code player} is null
+     */
+    public abstract boolean isLegal(Square move, Player player);
+
+    /**
+     * Returns a list holding the legal moves that the {@code player} can
+     * do at the board position. When no moves are available to the player
+     * the method returns an empty list.
+     *
+     * @param player the player
+     * @return       the moves available to the player
+     * @throws NullPointerException if parameter {@code player} is null
+     */
+    public List<Square> legalMoves(final Player player) {
+        if (player == null) { throw new NullPointerException("parameter player must be not null."); }
+        final List<Square> legalMoves = new ArrayList<Square>();
+        for (final Square move : Square.values()) {
+            if (isLegal(move, player)) { legalMoves.add(move); }
+        }
+        return legalMoves;
+    }
+
+    /**
+     * Returns a new updated board to reflect move by player. This static factory executes a game move
+     * to the board and returns a new one, reflecting the move. The original board is not modified.
+     * <p>
+     * A null value for player is not allowed, a {@code NullPointerException} is thrown in such a case.
+     * <p>
+     * A null value for move is allowed, and moreover is the only valid value
+     * acceptable by the method, when the player has not any legal move.
+     * Otherwise a null move is forbidden, and a {@code NullPointerException} is risen.
+     * <p>
+     * The method does check if the move is legal. It throws an {@code IllegalArgumentException} in case it is not.
+     *
+     * @param  move   the board square where to put the disk
+     * @param  player the disk color to put on the board
+     * @return        a new {@code Board} reflecting the move made
+     * @throws NullPointerException     if parameter {@code move} or {@code player} is null
+     * @throws IllegalArgumentException if the {@code move} by {@code player} is illegal
+     */
+    public abstract Board makeMove(Square move, Player player);
 
     /**
      * Returns a formatted string showing a 2d graphical represention of the board.
@@ -275,6 +294,34 @@ public abstract class AbstractBoard implements Board {
             hashCode = result;
         }
         return hashCode;
+    }
+
+    /**
+     * The {@code writeReplace()} method for the serialization proxy pattern.
+     * <p>
+     * The method return a newly created instance of the class {@code EnumMapBoard.SerializationProxy}.
+     * This instance is then serialized instead of the actual board object.
+     * <p>
+     * See the book: <i>"Bloch, Joshua. Effective Java Second Edition. Addison-Wesley, 2008"</i>.
+     *
+     * @return a new seialization proxy for the board object
+     */
+    Object writeReplace() {
+        return new SerializationProxy(this);
+    }
+
+    /**
+     * The {@code readObject()} method for the serialization proxy pattern.
+     * <p>
+     * The method cannot be invoked. It always throws a new exception.
+     * <p>
+     * See the book: <i>"Bloch, Joshua. Effective Java Second Edition. Addison-Wesley, 2008"</i>.
+     *
+     * @param stream the object input stream
+     * @throws InvalidObjectException always
+     */
+    private void readObject(final ObjectInputStream stream) throws InvalidObjectException {
+        throw new InvalidObjectException("Proxy required");
     }
 
 }
