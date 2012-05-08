22719b47c937c59ceb74591cc1c139699fff6701
Roberto Corradini <rob_corradini@yahoo.it>
2012-05-08 07:31:52 +0200

    Board and AbstractBoard javadocs are almost done.

--- java/src/rcrr/reversi/AbstractBoard.java
+++ java/src/rcrr/reversi/AbstractBoard.java
@@ -31,6 +31,18 @@ import java.io.InvalidObjectException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 
+/**
+ * The class provides an implementation for most methods defined by the {@code Board} interface.
+ * <p>
+ * It also gives the serialization machinery for a format common for all the class that derive from it.
+ * In order to use this service the subclass has to implement the {@code Serializable} interface and
+ * to define the {@code writeReplace()} method. For instance a simple implementation is:
+ * <pre>
+ *    Object writeReplace() {
+ *        return super.writeReplace();
+ *    }
+ * </pre>
+ */
 public abstract class AbstractBoard implements Board {
 
     /**
@@ -39,7 +51,7 @@ public abstract class AbstractBoard implements Board {
     private static final class SerializationProxy implements Serializable {
 
         /** The serialVersionUID requested by the specification for serialization. */
-        private static final long serialVersionUID = 2193788367767667846L;;
+        private static final long serialVersionUID = 2193788367767667846L;
 
         /**
          * The bitboard field.
@@ -76,6 +88,9 @@ public abstract class AbstractBoard implements Board {
 
     }
 
+    /** Class constructor. */
+    AbstractBoard() { }
+
     /** Prime number 17. */
     private static final int PRIME_NUMBER_17 = 17;
 
@@ -94,165 +109,9 @@ public abstract class AbstractBoard implements Board {
     }
 
     /**
-     * Returns the disk count for the color.
-     *
-     * @param color the color for which the disk count is computed
-     * @return      the disk count
-     * @throws NullPointerException if parameter {@code color} is null
+     * {@inheritDoc}
      */
     public abstract int countPieces(SquareState color);
-
-    /**
-     * Returns the {@code SquareState} value for the given board's square.
-     * <p>
-     * When {@code square} is {@code null} the method has to return {@code SquareState.OUTER} value.
-     *
-     * @param  square the board square to retrieve the state value
-     * @return        the square state
-     */
-    public abstract SquareState get(Square square);
-
-    /**
-     * Returns if the player has any legal move given the board state.
-     * <p>
-     * Parameter {@code player} must be not {@code null}.
-     *
-     * @param player the player
-     * @return       {@code true} if the player has any legal move, otherwise {@code false}
-     * @throws NullPointerException if parameter {@code player} is null
-     */
-    public boolean hasAnyLegalMove(final Player player) {
-        if (player == null) { throw new NullPointerException("parameter player must be not null."); }
-        boolean hasAnyLegalMove = false;
-        for (Square move : Square.values()) {
-            if (isLegal(move, player)) {
-                hasAnyLegalMove = true;
-                break;
-            }
-        }
-        return hasAnyLegalMove;
-    }
-
-    /**
-     * Returns true if either black or white player has any legal move.
-     *
-     * @return {@code true} if either player has a legal move
-     */
-    public boolean hasAnyPlayerAnyLegalMove() {
-        boolean hasAnyPlayerAnyLegalMove = false;
-        for (Player player : Player.values()) {
-            if (hasAnyLegalMove(player)) {
-                hasAnyPlayerAnyLegalMove = true;
-                break;
-            }
-        }
-        return hasAnyPlayerAnyLegalMove;
-    }
-
-    /**
-     * Returns the boolean value telling if the move, done by the specified player, is legal.
-     * <p>
-     * Parameter {@code move} must be not {@code null}.
-     * Parameter {@code player} must be not {@code null}.
-     *
-     * @param move   the square where to put the new disk
-     * @param player the player moving
-     * @return       true if the move is legal, otherwise false
-     * @throws NullPointerException if parameter {@code move} or {@code player} is null
-     */
-    public abstract boolean isLegal(Square move, Player player);
-
-    /**
-     * Returns a list holding the legal moves that the {@code player} can
-     * do at the board position. When no moves are available to the player
-     * the method returns an empty list.
-     *
-     * @param player the player
-     * @return       the moves available to the player
-     * @throws NullPointerException if parameter {@code player} is null
-     */
-    public List<Square> legalMoves(final Player player) {
-        if (player == null) { throw new NullPointerException("parameter player must be not null."); }
-        final List<Square> legalMoves = new ArrayList<Square>();
-        for (final Square move : Square.values()) {
-            if (isLegal(move, player)) { legalMoves.add(move); }
-        }
-        return legalMoves;
-    }
-
-    /**
-     * Returns a new updated board to reflect move by player. This static factory executes a game move
-     * to the board and returns a new one, reflecting the move. The original board is not modified.
-     * <p>
-     * A null value for player is not allowed, a {@code NullPointerException} is thrown in such a case.
-     * <p>
-     * A null value for move is allowed, and moreover is the only valid value
-     * acceptable by the method, when the player has not any legal move.
-     * Otherwise a null move is forbidden, and a {@code NullPointerException} is risen.
-     * <p>
-     * The method does check if the move is legal. It throws an {@code IllegalArgumentException} in case it is not.
-     *
-     * @param  move   the board square where to put the disk
-     * @param  player the disk color to put on the board
-     * @return        a new {@code Board} reflecting the move made
-     * @throws NullPointerException     if parameter {@code move} or {@code player} is null
-     * @throws IllegalArgumentException if the {@code move} by {@code player} is illegal
-     */
-    public abstract Board makeMove(Square move, Player player);
-
-    /**
-     * Returns a formatted string showing a 2d graphical represention of the board.
-     *
-     * @return a string being a 2d representation of the board
-     */
-    public String printBoard() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("    a b c d e f g h ");
-        for (final Row r : Row.values()) {
-            sb.append("\n ").append(r.label()).append("  ");
-            for (final Column c : Column.values()) {
-                String p = get(Square.getInstance(r, c)).symbol();
-                sb.append(p).append(" ");
-            }
-        }
-        sb.append("\n");
-        return sb.toString();
-    }
-
-    /**
-     * Returns a formatted string showing a 2d graphical composed view
-     * of the board and the disk count.
-     * <p>
-     * The method joins the printBoard() and the printCount() output,
-     * setting the second on the right of the first board's row.
-     *
-     * @return a string being a 2d representation of the board with the disk count
-     */
-    public String printBoardWithCount() {
-        final StringBuilder sbBoardWithCount = new StringBuilder();
-        final String sBoard = printBoard();
-        final String sCount = printCount();
-        final String[] lines = sBoard.split("\n");
-        for (int i = 0; i < lines.length; i++) {
-            String line = lines[i];
-            sbBoardWithCount.append(line);
-            if (i == 0) { sbBoardWithCount.append(sCount); }
-            sbBoardWithCount.append("\n");
-        }
-        return (sbBoardWithCount.toString());
-    }
-
-    /**
-     * Returns a formatted string, giving the two player disk count and their difference.
-     *
-     * @return a string showing the two player's count
-     */
-    public String printCount() {
-        final int cb = countPieces(SquareState.BLACK);
-        final int cw = countPieces(SquareState.WHITE);
-        final int cd = cb - cw;
-        return "[@=" + cb + " 0=" + cw + " (" + cd + ")]";
-    }
 
     /**
      * Returns true if the specified object is equal to this board.
@@ -274,6 +133,40 @@ public abstract class AbstractBoard implements Board {
     }
 
     /**
+     * {@inheritDoc}
+     */
+    public abstract SquareState get(Square square);
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean hasAnyLegalMove(final Player player) {
+        if (player == null) { throw new NullPointerException("parameter player must be not null."); }
+        boolean hasAnyLegalMove = false;
+        for (Square move : Square.values()) {
+            if (isLegal(move, player)) {
+                hasAnyLegalMove = true;
+                break;
+            }
+        }
+        return hasAnyLegalMove;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean hasAnyPlayerAnyLegalMove() {
+        boolean hasAnyPlayerAnyLegalMove = false;
+        for (Player player : Player.values()) {
+            if (hasAnyLegalMove(player)) {
+                hasAnyPlayerAnyLegalMove = true;
+                break;
+            }
+        }
+        return hasAnyPlayerAnyLegalMove;
+    }
+
+    /**
      * Returns a hash code for this board.
      *
      * @return a hash code for this board
@@ -288,6 +181,72 @@ public abstract class AbstractBoard implements Board {
             hashCode = result;
         }
         return hashCode;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public abstract boolean isLegal(Square move, Player player);
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<Square> legalMoves(final Player player) {
+        if (player == null) { throw new NullPointerException("Parameter player must be not null."); }
+        final List<Square> legalMoves = new ArrayList<Square>();
+        for (final Square move : Square.values()) {
+            if (isLegal(move, player)) { legalMoves.add(move); }
+        }
+        return legalMoves;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public abstract Board makeMove(Square move, Player player);
+
+    /**
+     * {@inheritDoc}
+     */
+    public String printBoard() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("    a b c d e f g h ");
+        for (final Row r : Row.values()) {
+            sb.append("\n ").append(r.label()).append("  ");
+            for (final Column c : Column.values()) {
+                String p = get(Square.getInstance(r, c)).symbol();
+                sb.append(p).append(" ");
+            }
+        }
+        sb.append("\n");
+        return sb.toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String printBoardWithCount() {
+        final StringBuilder sbBoardWithCount = new StringBuilder();
+        final String sBoard = printBoard();
+        final String sCount = printCount();
+        final String[] lines = sBoard.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+            sbBoardWithCount.append(line);
+            if (i == 0) { sbBoardWithCount.append(sCount); }
+            sbBoardWithCount.append("\n");
+        }
+        return (sbBoardWithCount.toString());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String printCount() {
+        final int cb = countPieces(SquareState.BLACK);
+        final int cw = countPieces(SquareState.WHITE);
+        final int cd = cb - cw;
+        return "[@=" + cb + " 0=" + cw + " (" + cd + ")]";
     }
 
     /**
