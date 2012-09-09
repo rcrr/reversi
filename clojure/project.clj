cc4d4af06b4f521142cf4227b722fa87288762a8
Roberto Corradini <rob_corradini@yahoo.it>
2012-09-09 17:39:59 +0200

    Clojure code is basically working, a few thing have to be carried out. See the TODO list.

--- clojure/project.clj
+++ clojure/project.clj
@@ -2,4 +2,5 @@
             :description "Reversi board program written in clojure"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [org.clojure/math.numeric-tower "0.0.1"]
-                           [org.clojure/algo.generic "0.1.0"]])
+                           [org.clojure/algo.generic "0.1.0"]]
+            :aot [rcrr.reversi.GameOverException])
