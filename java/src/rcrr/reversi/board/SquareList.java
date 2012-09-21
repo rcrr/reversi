0fb3444c99e41d2fe3320ac9a1c8e8e9989fe066
Roberto Corradini <rob_corradini@yahoo.it>
2012-09-21 07:51:10 +0200

    Added skeleton for SqareList. All the method have to be implemented.

---
+++ java/src/rcrr/reversi/board/SquareList.java
@@ -0,0 +1,129 @@
+/*
+ *  SquareList.java
+ *
+ *  Copyright (c) 2012 Roberto Corradini. All rights reserved.
+ *
+ *  This file is part of the reversi program
+ *  http://github.com/rcrr/reversi
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation; either version 3, or (at your option) any
+ *  later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ *  or visit the site <http://www.gnu.org/licenses/>.
+ */
+
+package rcrr.reversi.board;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Iterator;
+import java.util.Collection;
+
+/**
+ * A {@code List} implementation.
+ */
+public final class SquareList<E> implements List<E> {
+
+    public int size() {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public boolean contains(final Object o) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public Iterator<E> iterator() {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public Object[] toArray() {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public <T> T[] toArray(final T[] a) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public boolean containsAll(final Collection<?> c) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public boolean addAll(final int index, final Collection<? extends E> c) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public boolean addAll(final Collection<? extends E> c) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public boolean removeAll(final Collection<?> c) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public boolean retainAll(final Collection<?> c) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public void clear() {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public E get(final int index) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public E set(final int index, final E element) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public void add(final int index, final E element) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public boolean add(final E element) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public E remove(final int index) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public boolean remove(final Object o) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public int indexOf(final Object o) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public int lastIndexOf(final Object o) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public ListIterator<E> listIterator() {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public ListIterator<E> listIterator(final int index) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+    public List<E> subList(final int fromIndex, final int toIndex) {
+        throw new UnsupportedOperationException("Should be implemented.");
+    }
+
+}
