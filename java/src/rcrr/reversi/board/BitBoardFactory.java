308fd38881d44f934fcd5c035a160ecf1a0eb5dd
Roberto Corradini <rob_corradini@yahoo.it>
2012-10-01 19:56:42 +0200

    BitBoard0 now uses shift instead of neighbor. A strong semplification has been executed.

--- java/src/rcrr/reversi/board/BitBoardFactory.java
+++ java/src/rcrr/reversi/board/BitBoardFactory.java
@@ -38,7 +38,7 @@ public final class BitBoardFactory extends AbstractBoardFactory {
      * {@inheritDoc}
      */
     public Board valueOf(final Map<Square, SquareState> squares) {
-        return BitBoard2.valueOf(squares);
+        return BitBoard0.valueOf(squares);
     }
 
 }
