83573af2ed216448c57619037dfb9e2d9c06ab2a
Roberto Corradini <rob_corradini@yahoo.it>
2012-12-15 19:07:37 +0100

    Started to work on endgame solver. ExactSearcher and relative test added.

---
+++ java/test/rcrr/reversi/endgame/ExactSolverTest.java
@@ -0,0 +1,77 @@
+/*
+ *  ExactSolverTest.java
+ *
+ *  Copyright (c) 2010, 2011, 2012 Roberto Corradini. All rights reserved.
+ *
+ *  This file is part of the reversi program
+ *  http://github.com/rcrr/reversi
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation; either version 3, or (at your option) any
+ *  later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ *  or visit the site <http://www.gnu.org/licenses/>.
+ */
+
+package rcrr.reversi.endgame;
+
+import rcrr.reversi.GamePosition;
+import rcrr.reversi.board.BoardBuilder;
+import rcrr.reversi.board.Player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * Test Suite for {@code ExactSolver} class.
+ */
+public class ExactSolverTest {
+
+    /**
+     *  FFO position #40, black to move, Turner vs Monnom, Bruxelles 1997.
+     */
+    final static GamePosition FFO_40 = new GamePosition.Builder()
+        .withBoard(new BoardBuilder()
+                   .withSquaresLiteral(2, 0, 0, 2, 2, 2, 2, 1,
+                                       0, 2, 2, 2, 2, 2, 2, 1,
+                                       2, 2, 1, 1, 2, 2, 2, 1,
+                                       2, 2, 1, 2, 2, 2, 1, 1,
+                                       2, 2, 2, 2, 2, 2, 1, 1,
+                                       0, 0, 0, 2, 2, 2, 2, 1,
+                                       0, 0, 0, 0, 2, 0, 0, 1,
+                                       0, 0, 0, 0, 0, 0, 0, 0)
+                   .build())
+        .withPlayer(Player.BLACK)
+        .build();
+
+    /** Class constructor. */
+    public ExactSolverTest() { }
+
+    /**
+     * Tests the {@code dummy()} method.
+     *
+     * @see ExactSolver#dummy()
+     */
+    @Test
+    public final void testDummy() {
+
+        System.out.printf("FF0_40:\nBlack to move, Turner vs Monnom, Bruxelles 1997.\n%s\n", FFO_40.board().printBoard());
+
+        assertThat("dummy() is true.",
+                   new ExactSolver().dummy(),
+                   is(true));
+    }
+
+}
