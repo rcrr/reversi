f30d4baf01d6193dce7a4ad1de922ae378022e71
Roberto Corradini <rob_corradini@yahoo.it>
2013-03-23 21:52:54 +0100

    Added some comments ad documentation to board.h.

--- c/src/board.h
+++ c/src/board.h
@@ -48,15 +48,50 @@ typedef enum {
 
 /**
  * @enum SquareState
+ * @brief The `SquareState` identifies the state, or as a synonym the "color",
+ * of each board square.
  */
 typedef enum {
-  EMPTY_SQUARE,
-  BLACK_SQUARE,
-  WHITE_SQUARE
+  EMPTY_SQUARE,   /**< An empty square. */
+  BLACK_SQUARE,   /**< A black piece. */
+  WHITE_SQUARE    /**< A white piece. */
 } SquareState;
 
 /**
  * @enum Square
+ * @brief Square is an enum that realize the base unit of the game board.
+ *
+ * Squares are represented by two characters, a letter and a numeric digit.
+ *
+ * For instance, let's take a square: D4.
+ * This symbol identifies the square at the cross of column d and row 4.
+ *
+ * Here is represented the collection of the 64 Square as them are
+ * organized in the game board:
+ * @code
+ * .    a    b    c    d    e    f    g    h
+ *   =========================================
+ * 1 = A1 = B1 = C1 = D1 = E1 = F1 = G1 = H1 =
+ *   =========================================
+ * 2 = A2 = B2 = C2 = D2 = E2 = F2 = G2 = H2 =
+ *   =========================================
+ * 3 = A3 = B3 = C3 = D3 = E3 = F3 = G3 = H3 =
+ *   =========================================
+ * 4 = A4 = B4 = C4 = D4 = E4 = F4 = G4 = H4 =
+ *   =========================================
+ * 5 = A5 = B5 = C5 = D5 = E5 = F5 = G5 = H5 =
+ *   =========================================
+ * 6 = A6 = B6 = C6 = D6 = E6 = F6 = G6 = H6 =
+ *   =========================================
+ * 7 = A7 = B7 = C7 = D7 = E7 = F7 = G7 = H7 =
+ *   =========================================
+ * 8 = A8 = B8 = C8 = D8 = E8 = F8 = G8 = H8 =
+ *   =========================================
+ * @endcode
+ * Has to be noticed that the sequence of the squares is organized by rows. It means that
+ * the square ordered list is as follow:
+ *
+ * @code (A1, B1, C1, D1, E1, F1, G1, H1, A2, ... H8) @endcode.
  */
 typedef enum {
   A1, B1, C1, D1, E1, F1, G1, H1,
@@ -106,18 +141,25 @@ typedef struct Board {
 
 /**
  * @brief A game position is a value object joining the board and the moving player.
- *
  */
 typedef struct GamePosition {
-  Board board;
+  Board *board;
   Player player;
 } GamePosition;
+
+
+
+/* player functions. */
 
 extern SquareState player_color(const Player p);
 
 extern Player player_opponent(const Player p);
 
 extern char *player_description(const Player p);
+
+
+
+/* board functions. */
 
 extern Board *new_board(const SquareSet b,
                         const SquareSet w
@@ -151,6 +193,11 @@ extern SquareSet board_empties(const Board *const b);
 extern SquareSet board_blacks(const Board *const b);
 
 extern SquareSet board_whites(const Board *const b);
+
+
+
+/* direction functions. */
+
 
 extern SquareSet direction_shift_square_set(const Direction dir,
                                             const SquareSet squares
