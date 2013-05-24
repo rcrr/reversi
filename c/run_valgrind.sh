e546b7e46c7681c2995d7c1d757bb210acbce2b2
Roberto Corradini <rob_corradini@yahoo.it>
2013-05-24 21:09:43 +0200

    Suppressions for valgrind have been added.

--- c/run_valgrind.sh
+++ c/run_valgrind.sh
@@ -4,5 +4,21 @@ PROG=./build/test/game_position_db_test
 ARGS=
 
 G_SLICE=always-malloc
-G_DEBUG=gc-friendly 
-valgrind -v --tool=memcheck --leak-check=full --num-callers=40 --log-file=valgrind.log --track-origins=yes --leak-check=full --show-reachable=yes $(which $PROG) $ARGS
+G_DEBUG=gc-friendly
+
+OPTIONS_PLUS=--gen-suppressions=all
+
+valgrind -v --tool=memcheck \
+            --leak-check=full \
+            --num-callers=40 \
+            --log-file=valgrind.log \
+            --track-origins=yes \
+            --leak-check=full \
+            --show-reachable=yes \
+            --suppressions=minimal.supp \
+              $(which $PROG) $ARGS
+
+
+# how to generate the suppression file ....
+# cat ./minimalraw.log | ./parse_valgrind_suppressions.sh > minimal.supp
+# valgrind --leak-check=full --show-reachable=yes --error-limit=no --gen-suppressions=all --log-file=minimalraw.log ./minimal
