353a689345254bff3d0322b82a1926befb47d081
Roberto Corradini <rob_corradini@yahoo.it>
2014-07-19 18:20:49 +0200

    Running tests from 01 to 40 took 30 minutes. Everything worked fine.

--- c/test/exact_solver_test.c
+++ c/test/exact_solver_test.c
@@ -109,17 +109,74 @@ const TestCase ffo_20_29[] =
     {NULL, 0, 0, {A1}}
   };
 
+/**
+ * @brief Expected results for test cases coming from French Federation Othello game positions, from number 30 to 39.
+ */
+const TestCase ffo_30_39[] =
+  {
+    {"ffo-30", 1,  +0, { G3 } }, // ffo-30;.bbb....b.bww...bbwbww..bwbwbw..bwwbwbbbbwwbbwb...wwwww..bbbbb..;b; G3:+0. G2:-12. E1:-16. F2:-18. F1:-22. G4:-22. H6:-24. B7:-24. G8:-28;
+    {"ffo-31", 1,  -2, { G6 } }, // ffo-31;.wwwww....wwww..wbbwww...bbbww..bbbbbbw.bbbwww.wb.wwww...wwwww..;b; G6:-2. G3:-4. G4:-8. G7:-14. H5:-14. G2:-16. G1:-30. G8:-32;
+    {"ffo-32", 1,  -4, { G3 } }, // ffo-32;..bb....w.bbwb..wwbww...wbwbwww.wwbbwwwbwwbbbwwb..bbbbwb..b..b.b;b; G3:-4. B7:-6. E1:-8. H4:-10. F3:-10. H3:-10. B2:-14. A7:-22;
+    {"ffo-33", 2,  -8, { E7, A3 } }, // ffo-33;.bbbbbbb..bwww....wbwwbb.wwbbwbb.wwwwwbb.b.bwwbb...w.b.b..wwww..;b; E7:-8. A3:-8. A6:-12. B2:-12. G7:-12. G2:-12. A4:-14. C6:-20. A5:-22. B3:-28;
+    {"ffo-34", 1,  -2, { C2 } }, // ffo-34;.............w.w.wwwwwwwwwwwwbwwwbbwwwbw.bbbwbww..bbbwbw..wbbbbw;b; C2:-2. D2:-6. E2:-6. A3:-10. A2:-10. F1:-12. G2:-14. G1:-16. B2:-20. B8:-26;
+    {"ffo-35", 1,  +0, { C7 } }, // ffo-35;..bbb.....bbbb.wwwbbwwwwwwwwwwbw.wwbbbbw.wwwbbbw...bwbb...b.....;w; C7:+0. D8:-8. H8:-8. B2:-12. G1:-14. E8:-20. B1:-20. F8:-24. F1:-32. H7:-32. G8:-38;
+    {"ffo-36", 1,  +0, { B7 } }, // ffo-36;...b.w....bbbw.bbbbbbbbbbwwbbwwbbwbwwwbbbbwwww.bb..wwww.........;w; B7:+0. B1:-2. E1:-4. C1:-6. G6:-8. G2:-10. A2:-22. B2:-24;
+    {"ffo-37", 1, -20, { G2 } }, // ffo-37;..wwww..w.wwww..wbbbwww.wbbwbw..wwbbwbb.wwbbbb..w.bbb.....bb.w..;b; G2:-20. G4:-22. B7:-22. H3:-22. G1:-30. H2:-42. B1:-48;
+    {"ffo-38", 1,  +4, { B2 } }, // ffo-38;..wwww....wwww...bwbbwwbwwbwwwwb.wwwwwbbbwwbbbbb..b.b...........;b; B2:+4. A5:+0. H2:-4. A3:-10. A7:-18. G2:-20. B7:-22. G1:-24. B1:-26;
+    {"ffo-39", 9, +64, { A8, B1, G1, G5, G6, C8, H3, E8, H4 } }, // ffo-39;w.wwww..bwbbwb..bwwwbbb.bwwwbb..bwwbwb..bwbbb...b.bb............;w; A8:+64. B1:+64. G1:+64. G5:+64. G6:+64. C8:+64. H3:+64. E8:+64. H4:+64. F7:+62. D8:+62. E7:+62. H2:+62. B8:+62. G2:+60. G4:+60. F6:+32;
+    {NULL, 0, 0, {A1}}
+  };
+
+const TestCase ffo_40_49[] =
+  {
+    {"ffo-40", 1, +38, { A2 } }, // ffo-40;w..wwwwb.wwwwwwbwwbbwwwbwwbwwwbbwwwwwwbb...wwwwb....w..b........;b;FFO position 40, black to move, Turner vs Monnom, Bruxelles 1997.;
+    {NULL, 0, 0, {A1}}
+  };
+
 /*
-ffo-30;.bbb....b.bww...bbwbww..bwbwbw..bwwbwbbbbwwbbwb...wwwww..bbbbb..;b; G3:+0. G2:-12. E1:-16. F2:-18. F1:-22. G4:-22. H6:-24. B7:-24. G8:-28;
-ffo-31;.wwwww....wwww..wbbwww...bbbww..bbbbbbw.bbbwww.wb.wwww...wwwww..;b; G6:-2. G3:-4. G4:-8. G7:-14. H5:-14. G2:-16. G1:-30. G8:-32;
-ffo-32;..bb....w.bbwb..wwbww...wbwbwww.wwbbwwwbwwbbbwwb..bbbbwb..b..b.b;b; G3:-4. B7:-6. E1:-8. H4:-10. F3:-10. H3:-10. B2:-14. A7:-22;
-ffo-33;.bbbbbbb..bwww....wbwwbb.wwbbwbb.wwwwwbb.b.bwwbb...w.b.b..wwww..;b; E7:-8. A3:-8. A6:-12. B2:-12. G7:-12. G2:-12. A4:-14. C6:-20. A5:-22. B3:-28;
-ffo-34;.............w.w.wwwwwwwwwwwwbwwwbbwwwbw.bbbwbww..bbbwbw..wbbbbw;b; C2:-2. D2:-6. E2:-6. A3:-10. A2:-10. F1:-12. G2:-14. G1:-16. B2:-20. B8:-26;
-ffo-35;..bbb.....bbbb.wwwbbwwwwwwwwwwbw.wwbbbbw.wwwbbbw...bwbb...b.....;w; C7:+0. D8:-8. H8:-8. B2:-12. G1:-14. E8:-20. B1:-20. F8:-24. F1:-32. H7:-32. G8:-38;
-ffo-36;...b.w....bbbw.bbbbbbbbbbwwbbwwbbwbwwwbbbbwwww.bb..wwww.........;w; B7:+0. B1:-2. E1:-4. C1:-6. G6:-8. G2:-10. A2:-22. B2:-24;
-ffo-37;..wwww..w.wwww..wbbbwww.wbbwbw..wwbbwbb.wwbbbb..w.bbb.....bb.w..;b; G2:-20. G4:-22. B7:-22. H3:-22. G1:-30. H2:-42. B1:-48;
-ffo-38;..wwww....wwww...bwbbwwbwwbwwwwb.wwwwwbbbwwbbbbb..b.b...........;b; B2:+4. A5:+0. H2:-4. A3:-10. A7:-18. G2:-20. B7:-22. G1:-24. B1:-26;
-ffo-39;w.wwww..bwbbwb..bwwwbbb.bwwwbb..bwwbwb..bwbbb...b.bb............;w; A8:+64. B1:+64. G1:+64. G5:+64. G6:+64. C8:+64. H3:+64. E8:+64. H4:+64. F7:+62. D8:+62. E7:+62. H2:+62. B8:+62. G2:+60. G4:+60. F6:+32;
+
+ffo-41;.wwwww....wwwwb..wwwwww.bbbbbww..bbwwb..wwbwbb....wbbw...www..w.;b;FFO position 41, black to move, Eclipse vs Logistello, Internet (7-12) 1996.;
+ffo-42;..www.......bb.wwwwwwbww.wwwwbwwb.wwwbbw...wwbww...wwwbw..wwww..;b;FFO position 42, black to move, Penloup vs Shaman, Cambridge 1998.;
+ffo-43;..bbbbb...bbbb...wwwbb...wwbbbb..wwbbbw.wwwwbww....bwb....bbbbb.;w;FFO position 43, white to move, Brightwell vs Suekuni, WC 1997 (final).;
+ffo-44;..w.b.w...w.bw.w.wwbbbwwwwwwbbbwwwwwbb..bbwwbw....bbbb.....bbb..;w;FFO position 44, white to move, Shaman vs Tastet, WC 1995.;
+ffo-45;...bbbb.b.bbbw..bbwbww..bbbwbw..bbwbbw...wbbbww.w.wwww......ww..;b;FFO position 45, black to move, Tamenori vs Shaman, WC 1995 (final).;
+ffo-46;...bbb....wwwb....wwwbb..wwwwbbb..wwwwbb..wbwbbb..bbww...bbbb.w.;b;FFO position 46, black to move, Caspard vs Juhem, FC 1994.;
+ffo-47;.wwwww....wwww...wwwwb..bbbbbb...wbwwb..wwwbwb....wwbb....bbbb..;w;FFO position 47, white to move, Brightwell vs Tastet, WC 1997.;
+ffo-48;.....b..b.bbb...bbbbww..bwbwwbb.bwwbbb..bwwbb.....wwwb...bbbbbb.;w;FFO position 48, white to move, Brightwell vs Tastet, Paris Open 1997.;
+ffo-49;..wb.w....bbww..wwwwwbb.wwwwwb..wwwbwbb.wwwwbb.....wwb....b.w...;b;FFO position 49, black to move, Lazard vs Cali, FC 1991.;
+
+ffo-50;....b.....bbb....wwwbwww.wwwbwww.wbwbwbw.wwbbwww..wwbw....w..w..;b; D8:+10. H7:+6. A4:+2. B2:+2. G7:-2. G8:-2. A2:-4. A5:-6. A7:-6. G2:-6. H2:-6. A6:-8. A3:-10. B8:-14. B7:-18.;
+ffo-51;....w.b......b.....bbbw.wbbbbbww.bbwwbwwbbwbbbww..wwww.w....ww..;w; E2:+6. A3:+6. F1:+4. G7:+2. C2:+0. B3:-2. D2:-4. G2:-6. C3:-10. A5:-14.;
+ffo-52;...b.......wb..b..bwwbbbbbbwbbbbbbbwwbbbbbbwwwbb..bw...b........;w; A3:+0. E1:-2. B3:-2. F2:-8. F1:-12. B8:-12. G2:-16. B7:-18. B2:-22. A7:-26.;
+ffo-53;....ww.....www...bbbbwww..bbwwbw.bbbbbww..wwwbww..b.wb.w.....b..;b; D8:-2. C1:-4. E8:-6. D1:-8. G1:-8. G7:-12. B6:-12. B7:-14. G2:-18. H2:-20. D7:-22.;
+ffo-54;..www...bbww....bbbbwwwwbbbbwb..bbbwbb..bbwww......www.....w....;b; C7:-2. F8:-4. F2:-4. C8:-4. F6:-10. G8:-12. B1:-14. E2:-16. H2:-20. E8:-22.;
+ffo-55;........b.b......bbbbwwwwwbwbb..wwwbbbb.wwbbbb..w.wwwb.....ww...;w; G6:+0. B7:+0. E2:+0. G4:+0. F2:-2. H4:-2. D2:-2. C1:-4. H5:-4. F8:-6. D1:-12. G7:-16. A3:-16. B2:-28.;
+ffo-56;..bbbbb...bbbb...wwwbb...wwbwb...wbbbbb.wwwwwbw....wbb..........;w; H5:+2. F8:-8. G3:-12. E8:-14. G4:-20. G8:-28. G7:-32. G2:-34. B1:-40.;
+ffo-57;...................bbwww..bbbwww..bbwbww.wwwbbbw..wbww.w.wwwww..;b; A6:-10. F2:-12. B5:-16. A7:-18. G7:-20. G2:-20. B7:-22. G8:-52.;
+ffo-58;..bwww....www....wwwbww..wwwwbw..wbwbbb.wwbbbb....b.bb..........;b; G1:+4. A5:-6. B2:-10. H2:-18. A4:-22. A3:-22. F2:-22. A7:-24. H4:-26. B1:-26. A2:-28. H3:-36. G2:-38.;
+ffo-59;.......................w..wwwww...wwwwwbwwwwbbbb..bbwwbb..bb.w.b;b; H4:+64. G8:+64. E8:+64. A5:+62. B3:+54. F3:+52. B5:+44. D3:+38. E3:+36. G3:+34. C3:+30.;
+
+ffo-60;...wwww....www....bwbwbb..bwwbbb..bwwbbb..bwwwbb..wbbb.b..bbbb..;b; C2:+20. B8:+18. C1:+10. B6:+0. G7:-10. G2:-12. B7:-18.;
+ffo-61;.bbbb...b.bbwb..bbbbwbb.bwwbwwwwbwwwwww.bbwwww..b...w...........;w; H3:-14. G1:-14. F1:-16. B7:-22. H2:-30. B2:-34. G2:-46.;
+ffo-62;..wwww....wwbb....wbbbbbbbwbbwww.bbbbww.wbbbbbbw....b...........;w; E8:+28. D7:+24. H7:+24. F7:+18. C7:+14. D8:+10. A5:-2. G7:-4. H2:-8. G2:-20.;
+ffo-63;..b.......b.b....wbbbb...wbbbbw.wwbwbwwwwwwwwbw...bwbb.....bbbb.;w; F2:-2. B8:-4. D2:-8. C8:-10. E1:-10. B7:-12. F1:-14. G3:-16. D1:-18. G2:-18. G7:-24. B1:-28.;
+ffo-64;..w..b....w..b.w.wwbbbww..bbbbbw..bbwwww.bbbbbb...bbb....b.www..;w; B4:+20. E1:+18. E2:+18. B5:+18. D2:+10. A5:+8. F7:+6. C8:-6. G7:-12. H7:-16.;
+ffo-65;....ww....wwwwb...wbbbb.w.wbbbb..wwbbwb.bbwbbbb...wwww.......w..;b; G1:+10. C8:+8. A5:+8. B7:+6. D8:+4. B3:+2. D1:+0. B1:+0. E8:-4. B4:-6. C1:-8. B8:-10. G8:-14. B2:-30.;
+ffo-66;.www....b.wbb...bbwbbww.bwbbww..bbwwww..bbwwww....www.....w.....;b; H3:+30. G4:+28. F8:+26. G5:+18. F7:+16. G6:+12. G7:+10. D8:+6. B2:-14. E8:-16.;
+ffo-67;.bbbbb....bwbb..wwwbwbw..wwwbwww.wwwbbw...wwwb.w...wb...........;b; H3:+22. C8:+20. A6:+20. B6:+14. D8:+14. H5:+12. B2:+12. A4:+12. C7:+6. B7:+2. A5:-4. H2:-14.;
+ffo-68;...www....wwww....wbbwwb.wwbbwb..wwbbbb..bwwbb....www........w..;b; E8:+28. A5:+26. A6:+24. A4:+24. H2:+22. C8:+22. B1:+22. C1:+20. B3:+18. G2:+10. B2:+8. B7:+8. B8:+8. D8:+4. G1:-4.;
+ffo-69;..wwww.....www...wwwww..bbwbbww..wbwbww.wbbbbbb...b.b...........;b; H3:+0. H5:-2. A2:-2. A5:-4. G2:-4. C2:-12. H4:-12. B2:-12. G3:-12. G1:-26.;
+ffo-70;...b....b.bbb...bbbb....bbbwww..bbbbww..bbwwbbb.b.wwbb....w.....;b; E3:-24. E8:-26. D8:-26. G5:-26. F3:-44. B7:-48. G4:-56.;
+ffo-71;..................bbbbb..bbbbbw..wbbbwwb..wbwbbb..wwbb.b...bbbb.;w; D2:+20. F2:+18. B3:+16. A4:+10. H2:+10. E2:+4. G2:-22. G7:-22. C2:-30.;
+ffo-72;...w......wwbb...bbwbbb.bbbbwwbb.bbbbww...bbbww....bb.......b...;w; E1:+24. A3:+22. A6:+22. C8:+18. F1:+16. B2:+16. C7:+16. H3:+16. B6:+14. D8:+10. A5:+10. B7:+6. H5:+4. H2:+4. G2:-14.;
+ffo-73;..b..b....bbb...wwbbbb...wwbbb...wbwbbw.wwwbbbb.w..wbw..........;w; G4:-4. D8:-6. H5:-6. H6:-8. F8:-8. E1:-12. G3:-12. G2:-12. F2:-20. G7:-22. D1:-22.;
+ffo-74;....b.....wbbw.b..wbwwbb.wwbbwbb..wbbw.b..bbww....bwww.......w..;w; F1:-30. B5:-32. C1:-32. B6:-36. D1:-42. B8:-44. B7:-44. C8:-44.;
+ffo-75;....w.......ww....bbwb.w.bbbwbww..wwwww...wwwbwb..wwwwb......w..;b; D2:+14. H5:+12. D1:+8. B6:+4. F1:+0. C8:+0. H7:-2. D8:-4. G1:-6. G8:-6. B8:-10. G3:-16. B7:-34.;
+ffo-76;...w......ww.w.....wwwb.wwwwwwb..bbbbwbb..wwwwww..www.......w...;b; A3:+32. F7:+30. E1:+30. C1:+30. H7:+28. F8:+28. E2:+20. C3:+14. D8:+14. G7:+14. B8:+12. G2:+12. B7:+12. B3:+12. C8:+10. G1:-4.;
+ffo-77;..w.wb..b.www...bbwww...bbwbwwww.wwwww..w.b.w.....wb............;b; B7:+34. C8:+30. B6:+26. D6:+24. B2:+22. D1:+14. F2:+12. F6:+12. F3:+4. H3:+0. F7:-4.;
+ffo-78;....w.....wwww...wwwb.b.wwbwbbbb.bwwb...bwww.b....ww.......w....;b; F1:+8. A7:+6. C8:+4. A2:+2. A3:+2. E8:-2. C1:-2. G1:-8. E6:-10. B8:-10. B7:-18. B2:-20. B1:-22.;
+ffo-79;..............b.....w.bb...wwwb.wwwwbwbb..wwwwww..w.ww.w....ww..;b; D7:+64. D8:+62. H8:+56. C3:+30. B8:+16. C4:+14. E2:+12. D2:+12. G7:-2.;
+
  */
 
 
@@ -135,6 +192,14 @@ game_position_solve_ffo_01_19_test (GamePositionDbFixture *fixture,
 
 static void
 game_position_solve_ffo_20_29_test (GamePositionDbFixture *fixture,
+                                    gconstpointer          test_data);
+
+static void
+game_position_solve_ffo_30_39_test (GamePositionDbFixture *fixture,
+                                    gconstpointer          test_data);
+
+static void
+game_position_solve_ffo_40_49_test (GamePositionDbFixture *fixture,
                                     gconstpointer          test_data);
 
 
@@ -191,6 +256,18 @@ main (int   argc,
                 (gconstpointer) NULL,
                 gpdb_fixture_setup,
                 game_position_solve_ffo_20_29_test,
+                gpdb_fixture_teardown);
+    g_test_add ("/exact_solver/game_position_solve_ffo_30_39_test",
+                GamePositionDbFixture,
+                (gconstpointer) NULL,
+                gpdb_fixture_setup,
+                game_position_solve_ffo_30_39_test,
+                gpdb_fixture_teardown);
+    g_test_add ("/exact_solver/game_position_solve_ffo_40_49_test",
+                GamePositionDbFixture,
+                (gconstpointer) NULL,
+                gpdb_fixture_setup,
+                game_position_solve_ffo_40_49_test,
                 gpdb_fixture_teardown);
   }
   
@@ -250,6 +327,22 @@ game_position_solve_ffo_20_29_test (GamePositionDbFixture *fixture,
 {
   GamePositionDb *db = fixture->db;
   run_test_case_array(db, ffo_20_29);
+}
+
+static void
+game_position_solve_ffo_30_39_test (GamePositionDbFixture *fixture,
+                                    gconstpointer          test_data)
+{
+  GamePositionDb *db = fixture->db;
+  run_test_case_array(db, ffo_30_39);
+}
+
+static void
+game_position_solve_ffo_40_49_test (GamePositionDbFixture *fixture,
+                                    gconstpointer          test_data)
+{
+  GamePositionDb *db = fixture->db;
+  run_test_case_array(db, ffo_40_49);
 }
 
 
