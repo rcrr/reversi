c9d652da5ad2c635dfed87f72818199e895e5959
Roberto Corradini <rob_corradini@yahoo.it>
2014-12-03 23:22:00 +0100

    Added template version of Shell sort.

--- c/src/sort_utils.c
+++ c/src/sort_utils.c
@@ -1024,3 +1024,36 @@ sms_semitrinkle (const sort_utils_compare_function cmp,
 /**
  * @endcond
  */
+
+
+void ssort(void  *base,
+           size_t nel,
+           size_t width,
+           int (*comp)(const void *, const void *))
+{
+  size_t wnel, gap, wgap, i, j, k;
+  char *a, *b, tmp;
+
+  wnel = width * nel;
+  for (gap = 0; ++gap < nel;)
+    gap *= 3;
+  while ((gap /= 3) != 0) {
+    wgap = width * gap;
+    for (i = wgap; i < wnel; i += width) {
+      for (j = i - wgap; ;j -= wgap) {
+        a = j + (char *)base;
+        b = a + wgap;
+        if ((*comp)(a, b) <= 0)
+          break;
+        k = width;
+        do {
+          tmp = *a;
+          *a++ = *b;
+          *b++ = tmp;
+        } while (--k);
+        if (j < wgap)
+          break;
+      }
+    }
+  }
+}
