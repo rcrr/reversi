2ecfdfb3bebd732eaac6caa462dfb9ed7a053aad
Roberto Corradini <rob_corradini@yahoo.it>
2015-10-18 09:59:42 +0200

    PVE refactoring: added skeleton files for bma and pve modules.

---
+++ c/src/principal_variation.c
@@ -0,0 +1,43 @@
+/**
+ * @file
+ *
+ * @brief Principal variation environment module.
+ *
+ * @details The principal variation refers to the particular variation that is the most advantageous
+ *          to the current player, assuming each other player will respond with the move that best
+ *          improves their own position.
+ *
+ *          All the symbols exported by the module adopt the `pve_` prefix.
+ *
+ * @par principal_variation.c
+ * <tt>
+ * This file is part of the reversi program
+ * http://github.com/rcrr/reversi
+ * </tt>
+ * @author Roberto Corradini mailto:rob_corradini@yahoo.it
+ * @copyright 2015 Roberto Corradini. All rights reserved.
+ *
+ * @par License
+ * <tt>
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3, or (at your option) any
+ * later version.
+ * \n
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * \n
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ * or visit the site <http://www.gnu.org/licenses/>.
+ * </tt>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "principal_variation.h"
