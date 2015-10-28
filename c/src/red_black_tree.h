c8085852221a3b9ba291451c25fc179b4a4511e5
Roberto Corradini <rob_corradini@yahoo.it>
2015-10-28 19:58:58 +0100

    PVE refactoring: added traverser documentation in red_black_tree.h.

--- c/src/red_black_tree.h
+++ c/src/red_black_tree.h
@@ -138,6 +138,8 @@ typedef struct rbt_node {
 
 /**
  * @brief Red-black tree data structure.
+ *
+ * @details See Wikipedia page at: https://en.wikipedia.org/wiki/Red-black_tree
  */
 typedef struct rbt_table {
   rbt_node_t         *root;                     /**< @brief Tree's root. */
@@ -150,6 +152,81 @@ typedef struct rbt_table {
 
 /**
  * @brief Red-black tree traverser structure.
+ *
+ * @details A `struct rbt_traverser` is a table “traverser” that allows the items in a table
+ *          to be examined. With a traverser, the items within a table can be enumerated in sorted
+ *          ascending or descending order, starting from either end or from somewhere in the middle.
+ *
+ * The user of the traverser declares its own instance of struct tbl traverser, typically as a
+ *    local variable. One of the traverser constructor functions described below can be used to
+ *    initialize it. Until then, the traverser is invalid. An invalid traverser must not be passed to
+ *    any traverser function other than a constructor.
+ *
+ * Seen from the viewpoint of a table user, a traverser has only one attribute: the current
+ *    item. The current item is either an item in the table or the “null item”, represented by a
+ *    null pointer and not associated with any item.
+ *
+ * Traversers continue to work when their tables are modified. Any number of insertions and
+ *    deletions may occur in the table without affecting the current item selected by a traverser,
+ *    with only a few exceptions:
+ *    - Deleting a traverser’s current item from its table invalidates the traverser (even if the
+ *      item is later re-inserted).
+ *    - Using the return value of rbt_probe() to replace an item in the table invalidates all
+ *      traversers with that item current, unless the replacement item has the same key data
+ *      as the original item (that is, the table’s comparison function returns 0 when the two
+ *      items are compared).
+ *    - Similarly, rbt_replace() invalidates all other traversers with the same item selected,
+ *      unless the replacement item has the same key data.
+ *    - Destroying a table with rbt_destroy() invalidates all of that table’s traversers.
+ *
+ * There is no need to destroy a traverser that is no longer needed. An unneeded traverser
+ *    can simply be abandoned.
+ *
+ * Traversers are managed by two classes of functions:
+ *     - _Constructors_ <br>
+ *       These functions initialize traversers. A traverser must be initialized with one of these
+ *       functions before it is passed to any other traverser function.<br>
+ *       All of these functions take a traverser to initialize as their first argument, and most take a
+ *       table to associate the traverser with as their second argument. These arguments are here
+ *       called `trav` and `table`. All, except rbt_t_init(), return the item to which `trav` is initialized,
+ *       using a null pointer to represent the null item. None of the arguments to these functions may ever be a null pointer.
+ *       - rbt_t_init(): Initializes `trav` to the null item in `table`.
+ *       - rbt_t_first(): Initializes `trav` to the least-valued item in `table`. If the table is empty, then
+ *         trav is initialized to the null item.
+ *       - rbt_t_last(): Same as rbt_t_first(), for the greatest-valued item in `table`.
+ *       - rbt_t_find(): Searches `table` for an item matching the one given. If one is found, initializes
+ *         `trav` with it. If none is found, initializes `trav` to the null item.
+ *       - rbt_t_insert(): Attempts to insert the given item into `table`. If it is inserted succesfully,
+ *         `trav` is initialized to its location. If it cannot be inserted because of a duplicate, the
+ *         duplicate item is set as `trav`’s current item. If there is a memory allocation error, `trav`
+ *         is initialized to the null item.
+ *       - rbt_t_copy(): Initializes `trav` to the same table and item as a second valid traverser.
+ *         Both arguments pointing to the same valid traverser is valid and causes no change in either.
+ *     - _Manipulators_ <br>
+ *       Each of these functions takes a valid traverser, here called `trav` , as its first argument, and
+ *       returns a data item. All but rbt_t_replace() can also return a null pointer that represents
+ *       the null item. All arguments to these functions must be non-null pointers.
+ *       - rbt_t_next(): Advances `trav` to the next larger item in its table. If `trav` was at the null
+ *         item in a nonempty table, then the smallest item in the table becomes current. If `trav`
+ *         was already at the greatest item in its table or the table is empty, the null item becomes
+ *         current. Returns the new current item.
+ *       - rbt_t_prev(): Advances `trav` to the next smaller item in its table. If `trav` was at the null
+ *         item in a nonempty table, then the greatest item in the table becomes current. If `trav`
+ *         was already at the lowest item in the table or the table is empty, the null item becomes
+ *         current. Returns the new current item.
+ *       - rbt_t_cur(): Returns `trav`’s current item.
+ *       - rbt_t_replace(): Replaces the data item currently selected in trav by the one provided.
+ *         The replacement item is subject to the same restrictions as for the same replacement
+ *         using rbt_probe(). The item replaced is returned. If the null item is current, the behavior
+ *         is undefined.
+ *
+ * Seen from the outside, the traverser treats the table as a circular arrangement of items. Moving clockwise in the circle
+ *    is equivalent, under our traverser, to moving to the next item with rbt_t_next(). Moving counterclockwise is equivalent
+ *    to moving to the previous item with rbt_t_prev().<br>
+ *    From this perspective, nodes are arranged from least to greatest in left to right order, and the null node lies in the
+ *    middle as a connection between the least and greatest nodes. Moving to the next node is the same as moving to the right
+ *    and moving to the previous node is motion to the left, except where the null node is concerned.
+ *
  */
 typedef struct rbt_traverser {
   rbt_table_t        *table;                    /**< @brief Tree being traversed. */
