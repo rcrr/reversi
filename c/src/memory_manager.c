dbb1add9740c5228fd2219f8c50d7892f63fd9e1
Roberto Corradini <rob_corradini@yahoo.it>
2015-11-26 07:44:07 +0100

    PVE refactoring: working on memory manager ...

--- c/src/memory_manager.c
+++ c/src/memory_manager.c
@@ -118,3 +118,75 @@ mem_block_free (mem_allocator_t *alloc,
   assert(alloc != NULL && block != NULL);
   free(block);
 }
+
+/* **** **** **** **** */
+
+
+/*
+ * Memory tracking policy.
+ *
+ * MT_TRACK and MT_NO_TRACK should be self-explanatory.
+ * MT_FAIL_COUNT takes an
+ * argument specifying after how many allocations further allocations should always fail.
+ * MT_FAIL_PERCENT takes an argument specifying an integer percentage of allocations to
+ * randomly fail.
+ *
+ * MT_SUBALLOC causes small blocks to be carved out of larger ones allocated with malloc().
+ * This is a good idea for two reasons: malloc() can be slow and malloc() can waste a lot of
+ * space dealing with the small blocks that Libavl uses for its node. Suballocation cannot be
+ * implemented in an entirely portable way because of alignment issues, but the test program
+ * here requires the user to specify the alignment needed, and its use is optional anyhow.
+ */
+enum mt_policy {
+  MT_TRACK,         /* Track allocation for leak detection. */
+  MT_NO_TRACK,      /* No leak detection. */
+  MT_FAIL_COUNT,    /* Fail allocations after a while. */
+  MT_FAIL_PERCENT,  /* Fail allocations randomly. */
+  MT_SUBALLOC       /* Suballocate from larger blocks. */
+};
+
+/*
+ * A memory block.
+ *
+ * The memory manager keeps track of allocated blocks using struct block.
+ *
+ * The next member of struct block is used to keep a linked list of all the currently allocated
+ * blocks. Searching this list is inefficient, but there are at least two reasons to do it this way,
+ * instead of using a more efficient data structure, such as a binary tree. First, this code is for
+ * testing binary tree routines—using a binary tree data structure to do it is a strange idea!
+ * Second, the ISO C standard says that, with few exceptions, using the relational operators
+ * (<, <=, >, >=) to compare pointers that do not point inside the same array produces
+ * undefined behavior, but allows use of the equality operators (==, !=) for a larger class of
+ * pointers.
+ *
+ */
+struct block {
+  struct block *next;   /* Next in linked list. */
+  int idx;              /* Allocation order index number. */
+  size_t size;          /* Size in bytes. */
+  size_t used;          /* MT SUBALLOC: amount used so far. */
+  void *content;        /* Allocated region. */
+};
+
+/* Indexes into arg[] within struct mt_allocator. */
+enum mt_arg_index {
+  MT_COUNT = 0,         /* MT_FAIL_COUNT: Remaining successful allocations. */
+  MT_PERCENT = 0,       /* MT_FAIL_PERCENT: Failure percentage. */
+  MT_BLOCK_SIZE = 0,    /* MT_SUBALLOC: Size of block to suballocate. */
+  MT_ALIGN = 1          /* MT_SUBALLOC: Alignment of suballocated blocks. */
+};
+
+/* Memory tracking allocator. */
+struct mt_allocator {
+  struct mem_allocator allocator;    /* Allocator. Must be first member. */
+  /* Settings. */
+  enum mt_policy policy;             /* Allocation policy. */
+  int arg[2];                        /* Policy arguments. */
+  int verbosity;                     /* Message verbosity level. */
+  /* Current state. */
+  struct block *head, *tail;         /* Head and tail of block list. */
+  int alloc_idx;                     /* Number of allocations so far. */
+  int block_cnt;                     /* Number of still-allocated blocks. */
+};
+
+/* pp 109 in PDF ... */
