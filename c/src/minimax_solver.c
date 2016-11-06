85a96b3e45dd3a0f4098f664d66ffc10510cb24a
Roberto Corradini <rob_corradini@yahoo.it>
2016-11-06 20:24:45 +0100

    Old variant fo minimax code has been removed .... still work in progress.

--- c/src/minimax_solver.c
+++ c/src/minimax_solver.c
@@ -50,15 +50,11 @@
  * Prototypes for internal functions.
  */
 
-static SearchNode *
-game_position_solve_impl (ExactSolution *const result,
-                          const GamePosition *const gp);
-
 static int
-game_position_solve_impl2 (ExactSolution *const result,
-                           Square *move,
-                           const GamePositionX *const gpx,
-                           const Square prev_move);
+game_position_solve_impl (ExactSolution *const result,
+                          Square *move,
+                          const GamePositionX *const gpx,
+                          const Square prev_move);
 
 
 
@@ -116,34 +112,18 @@ game_position_minimax_solve (const GamePositionX *const root,
     game_tree_log_open_h(log_env);
   }
 
-  bool new = true;
+  ExactSolution *result = NULL;
 
-    ExactSolution *result = NULL;
-    ExactSolution *result2 = NULL;
-  if (!new) {
-    result = exact_solution_new();
-
-    GamePosition *const root_gp = game_position_x_gpx_to_gp(root);
-    result->solved_game_position = game_position_clone(root_gp);
-    free(root_gp);
-
-    SearchNode *sn = game_position_solve_impl(result, result->solved_game_position);
-
-    result->pv[0] = sn->move;
-    result->outcome = sn->value;
-    search_node_free(sn);
-  } else {
-    result2 = exact_solution_new();
-    exact_solution_set_solved_game_position_x(result2, root);
-    Square best_move;
-    int v = game_position_solve_impl2(result2, &best_move, root, invalid_move);
-    result2->pv[0] = best_move;
-    result2->outcome = v;
-  }
+  result = exact_solution_new();
+  exact_solution_set_solved_game_position_x(result, root);
+  Square best_move;
+  int v = game_position_solve_impl(result, &best_move, root, invalid_move);
+  result->pv[0] = best_move;
+  result->outcome = v;
 
   game_tree_log_close(log_env);
 
-  return new ? result2 : result;
+  return result;
 }
 
 
@@ -205,18 +185,18 @@ make_move (const GamePositionX *const current,
 
 /*
  * TODO:
- * - Remove every dependency on GamePosition and Board objects.
- * - Remove pass special case.
- * - Avoid malloc/free calls.
- * - Use the game_tree_utils stack.
- * - Use the most advanced Kogge-Stone routines.
- * - Transform the code from recursion to iteration.
+ * -        Remove every dependency on GamePosition and Board objects.
+ * - [done] Remove pass special case.
+ * - [done] Avoid malloc/free calls.
+ * -        Use the game_tree_utils stack.
+ * - [done] Use the most advanced Kogge-Stone routines.
+ * -        Transform the code from recursion to iteration.
  */
 static int
-game_position_solve_impl2 (ExactSolution *const result,
-                           Square *move,
-                           const GamePositionX *const gpx,
-                           const Square prev_move)
+game_position_solve_impl (ExactSolution *const result,
+                          Square *move,
+                          const GamePositionX *const gpx,
+                          const Square prev_move)
 {
   Square *m;
   Square moves[32];
@@ -259,7 +239,7 @@ game_position_solve_impl2 (ExactSolution *const result,
 
   for (m = moves; m - moves < move_count; m++) {
     make_move(gpx, *m, &child_gpx);
-    v = - game_position_solve_impl2(result, &a_move, &child_gpx, *m);
+    v = - game_position_solve_impl(result, &a_move, &child_gpx, *m);
     if (v > best_value) {
       best_move = *m;
       best_value = v;
@@ -272,89 +252,6 @@ game_position_solve_impl2 (ExactSolution *const result,
   }
   *move = best_move;
   return best_value;
-}
-
-/**
- * @brief Recursive function used to traverse the game tree.
- *
- * @param [in] result a reference to the exact solution data structure
- * @param [in] gp     the game position to traverse
- * @return            a pointer to a new serch node structure
- */
-static SearchNode *
-game_position_solve_impl (ExactSolution *const result,
-                          const GamePosition *const gp)
-{
-  SearchNode *node  = NULL;
-  SearchNode *node2 = NULL;
-
-  result->node_count++;
-
-  const SquareSet moves = game_position_legal_moves(gp);
-
-  if (log_env->log_is_on) {
-    call_count++;
-    gp_hash_stack_fill_point++;
-    LogDataH log_data;
-    log_data.sub_run_id = 0;
-    log_data.call_id = call_count;
-    log_data.hash = game_position_hash(gp);
-    gp_hash_stack[gp_hash_stack_fill_point] = log_data.hash;
-    log_data.parent_hash = gp_hash_stack[gp_hash_stack_fill_point - 1];
-    log_data.blacks = (gp->board)->blacks;
-    log_data.whites = (gp->board)->whites;
-    log_data.player = gp->player;
-    gchar *json_doc = game_tree_log_data_h_json_doc(gp_hash_stack_fill_point, gp);
-    log_data.json_doc = json_doc;
-    game_tree_log_write_h(log_env, &log_data);
-    g_free(json_doc);
-  }
-
-  if (moves == empty_square_set) {
-    GamePosition *flipped_players = game_position_pass(gp);
-    if (game_position_has_any_legal_move(flipped_players)) {
-      node = search_node_negated(game_position_solve_impl(result, flipped_players));
-    } else {
-      result->leaf_count++;
-      node = search_node_new(pass_move, game_position_final_value(gp));
-    }
-    game_position_free(flipped_players);
-  } else {
-    // -- to be removed -start 1
-    SquareSet move_set;
-    int move_count;
-    Square move_array[32];
-    GamePositionX *gpx = game_position_x_gp_to_gpx(gp);
-    generate_move_array(&move_count, move_array, &move_set, gpx);
-    Square *move_2 = move_array;
-    if (move_set != moves) abort();
-    if (bit_works_bitcount_64_popcnt(moves) != move_count) abort();
-    // -- to be removed -end 1
-    node = search_node_new(-1, -65);
-    SquareSet remaining_moves = moves;
-    while (remaining_moves) {
-      const Square move = bit_works_bitscanLS1B_64(remaining_moves);
-      if (move != *move_2++) abort(); // -- to be removed 2
-      remaining_moves ^= 1ULL << move;
-      GamePosition *gp2 = game_position_make_move(gp, move);
-      node2 = search_node_negated(game_position_solve_impl(result, gp2));
-      game_position_free(gp2);
-      if (node2->value > node->value) {
-        search_node_free(node);
-        node = node2;
-        node->move = move;
-        node2 = NULL;
-      } else {
-        search_node_free(node2);
-      }
-    }
-  }
-
-  if (log_env->log_is_on) {
-    gp_hash_stack_fill_point--;
-  }
-
-  return node;
 }
 
 /**
