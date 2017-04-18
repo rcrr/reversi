24503f9e51cea9b07b00e3a46551a175ec6d0132
Roberto Corradini <rob_corradini@yahoo.it>
2017-04-18 20:44:17 +0200

    More tests added to es_pv unit test ...

--- c/test/es_pv_test.c
+++ c/test/es_pv_test.c
@@ -98,6 +98,16 @@ const TestCase ffo_05[] =
  * ffo-17: 78982c0796df321412e0874100e24fbb93c6e13404e2f8f23375616b12d014bc
  * ffo-18: 47d56b9524c44829d3d3135acccd95f2b80dd5b785a9f99f3b9132c05d6dad58
  * ffo-19: 407895f2e49e48cffdb879c023063ba51a6b32a952c1e990e6b63a7baafaddaf
+ * ffo-20: f50a8b973129512968d5853823892c6e2954aaf7504b4c2cab6b0538c709a63b
+ * ffo-21: 345d1108d154761edd3d2548a89855037fb15154777e3479c5df359eed6a46f5
+ * ffo-22: a37abd8c44bd99a4caeec61558cd3c94e82d01dafaabc3fce7fe9635465c32a3
+ * ffo-23: 75797e7a8c10bf4402c72d048d2d0a94934d35f051f863c8f8c56b3d714db35f
+ * ffo-24: 9a5f325dab822cff7e7810a4661d02ce5bb5de69ab9213d443df5f5c7873186f
+ * ffo-25: bd2a938c87556c7f0cec0840a3a895a61d5345837f5c8f75b0679d1a0112d0b6
+ * ffo-26: d1e162de3dc89d039eeabda515e5984827e65c7b6d3b971695dfdf35aafab3af
+ * ffo-27: 23fa980ff745e949cf5a32abb9be6a7b071c591f6ffef6c83e88251f5ef59f30
+ * ffo-28: 4d86c09d124bc7338ec9d66b616c85337a2d7cc0f3bcc1004a5cef7a73b57901
+ * ffo-29: 4f18d1a3f89bedfd288c8832cfad086781aa6388ae3ac2997d5782f0c9ae3d40
  *
  */
 
@@ -125,6 +135,24 @@ const TestCase ffo_01_19[] =
     { "ffo-17", 1,  +8, { F8 },     "78982c0796df321412e0874100e24fbb93c6e13404e2f8f23375616b12d014bc" }, // ffo-17;.wwwww....wbbw.bbbwwwbb.bbwbwbbwbbwwbwwwbbbbww.wb.bww....bbbb...;b; F8:+8. G2:+6. G6:-24. G1:-32. F7:-32. G7:-34. B2:-38.;
     { "ffo-18", 1,  -2, { G2 },     "47d56b9524c44829d3d3135acccd95f2b80dd5b785a9f99f3b9132c05d6dad58" }, // ffo-18;.bbb......wwwb..bwwwwwbbwbwbwwbbwbbwwwwwbbbwbwwb..wbbw...wwwww..;b; G2:-2. B7:-6. F1:-8. E1:-10. H7:-12. G8:-14. G7:-14. A2:-18. B2:-18.;
     { "ffo-19", 1,  +8, { B6 },     "407895f2e49e48cffdb879c023063ba51a6b32a952c1e990e6b63a7baafaddaf" }, // ffo-19;..wbbw..bwbbbb..bwwwwbbbbwwwbbbbb.wwwbbb..wwwwbb..bbwww....bbww.;b; B6:+8. H8:+4. B7:+0. G1:-6. B5:-16. H7:-16. B1:-24.;
+    {NULL, 0, 0, {A1}}
+  };
+
+/**
+ * @brief Expected results for test cases coming from French Federation Othello game positions, from number 20 to 29.
+ */
+const TestCase ffo_20_29[] =
+  {
+    {"ffo-20", 1,  +6, { H5 },         "f50a8b973129512968d5853823892c6e2954aaf7504b4c2cab6b0538c709a63b" }, // ffo-20;bbbwbbbbwbbbbbbbwwbbbbbbwwwbbbbbwwwbbww.wwwww...wwwwwww.wwwwwww.;b; H5:+6. G6:-2. F6:-4. H6:-10;
+    {"ffo-21", 1,  +0, { G5 },         "345d1108d154761edd3d2548a89855037fb15154777e3479c5df359eed6a46f5" }, // ffo-21;wwwwwwwwbwwbbb..bbwwbww.bwbwww..bwwwwb..bwwbww..bwwwww..bbbb....;w; G5:+0. G2:-2. G4:-4. G6:-6;
+    {"ffo-22", 1,  +2, { G8 },         "a37abd8c44bd99a4caeec61558cd3c94e82d01dafaabc3fce7fe9635465c32a3" }, // ffo-22;..wwww..b.wwwww.bbwwbwbbbwbwbbbbbbbwbbbb.bbwbwbb..wbbb.b....b...;w; G8:+2. A6:+0. F8:-4. A7:-4. H2:-4. B2:-6. D8:-8. B7:-14. G7:-26;
+    {"ffo-23", 1,  +4, { A2 },         "75797e7a8c10bf4402c72d048d2d0a94934d35f051f863c8f8c56b3d714db35f" }, // ffo-23;..w.......wwb...wwwbbbw.wwwwbwbbbbbwwbwbbbbbbwwbb.bbbbwb..bbbb..;b; A2:+4. D1:-20. H3:-20. B1:-30. G2:-30. E1:-30. F2:-34. G8:-34. B2:-36. H2:-38;
+    {"ffo-24", 1,  +0, { C3 },         "9a5f325dab822cff7e7810a4661d02ce5bb5de69ab9213d443df5f5c7873186f" }, // ffo-24;..w..w.....wwwb..b.bwbww..bbbwwwbbbbwwwwbbbwbbwwbbbbbb..bwbb.w..;w; C3:+0. B4:-4. C2:-8. E8:-12. G7:-14. H2:-16. G1:-24;
+    {"ffo-25", 2,  +0, { G1, A5 },     "bd2a938c87556c7f0cec0840a3a895a61d5345837f5c8f75b0679d1a0112d0b6" }, // ffo-25;....b......bbbw..wwwbbbbbwwwwbbw.bbwwbbwwwbwbbbbwwwbb...b.bbbb..;w; G1:+0. A5:+0. F1:-4. D1:-6. F7:-8. C2:-10. G7:-10. H2:-12. H7:-16;
+    {"ffo-26", 1,  +0, { D8 },         "d1e162de3dc89d039eeabda515e5984827e65c7b6d3b971695dfdf35aafab3af" }, // ffo-26;.wwwww....wbbw...wwwwbbw.wwwbwbb.wwbwwbb.bwbbwbb..w.bbbb..w....w;b; D8:+0. A6:-2. A4:-6. B7:-6. A5:-12. G1:-16. A2:-16. A3:-18. H2:-18. B8:-20. G2:-20. B2:-26;
+    {"ffo-27", 1,  -2, { B7 },         "23fa980ff745e949cf5a32abb9be6a7b071c591f6ffef6c83e88251f5ef59f30" }, // ffo-27;..bw.w....wwww..wwbwbbw.wwwwbbwwwwwbbwb.wbwbbbbb..bbbb....b.w.b.;b; B7:-2. E1:-4. B1:-6. H2:-10. H5:-10. B2:-12. A2:-14. H3:-28. G1:-28. G2:-28;
+    {"ffo-28", 3,  +0, { F1, B2, E1 }, "4d86c09d124bc7338ec9d66b616c85337a2d7cc0f3bcc1004a5cef7a73b57901" }, // ffo-28;..w.......www..b.bwwwwbbbbbbwbwb.bbwbwwbbbwbwwbb.wwwww.b...www..;b; F1:+0. B2:+0. E1:+0. B1:-4. F2:-6. G7:-6. D1:-12. C8:-20. G8:-22. B8:-28;
+    {"ffo-29", 1, +10, { G2 },         "4f18d1a3f89bedfd288c8832cfad086781aa6388ae3ac2997d5782f0c9ae3d40" }, // ffo-29;.wbbbb....wbbw..bbwwbwwwbbbwwbwwbbwwbwwwbbbbww.bb.bbw...........;b; G2:+10. A1:+4. G6:-10. H2:-12. F8:-12. E8:-12. G7:-24. G1:-24. B2:-30. F7:-34;
     {NULL, 0, 0, {A1}}
   };
 
@@ -187,6 +215,13 @@ main (int   argc,
     g_test_add("/es_pv/ffo_01_19",
                GamePositionDbFixture,
                (gconstpointer) ffo_01_19,
+               gpdb_ffo_fixture_setup,
+               game_position_es_solve_test,
+               gpdb_fixture_teardown);
+
+    g_test_add("/es_pv/ffo_20_29",
+               GamePositionDbFixture,
+               (gconstpointer) ffo_20_29,
                gpdb_ffo_fixture_setup,
                game_position_es_solve_test,
                gpdb_fixture_teardown);
