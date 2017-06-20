e7382c48460a15daf915b301ac939f0d61ee9aa6
Roberto Corradini <rob_corradini@yahoo.it>
2017-06-20 21:36:32 +0200

    Working on read_pve_dump ...

--- c/src/read_pve_dump.c
+++ c/src/read_pve_dump.c
@@ -50,62 +50,7 @@
 
 /* Static constants. */
 
-static const gchar *program_documentation_string =
-  "Description:\n"
-  "Read Principal Variation Environment dump is a program that load a binary dump file representation of a PVE.\n"
-  "\n"
-  "Details on Application Options:\n"
-  "\n"
-  "  -s, --print-summary\n"
-  "\n"
-  "  -i, --internals\n"
-  "    Shows the internal PVE data selected by the switches turned on, the structure is read and then activated, this\n"
-  "    last process translates all the memory references in the structure. The data shown is the memory view after translation.\n"
-  "    The available sections are:\n"
-  "    -00- PVE HEADER ... ... ... ... ... ... 0x0001, or ... 1\n"
-  "    -01- PVE INDEX  ... ... ... ... ... ... 0x0002, or ... 2\n"
-  "    -03- PVE PROPERTIES ... ... ... ... ... 0x0004, or ... 4\n"
-  "    -03- PVE STRUCTURE HEADER   ... ... ... 0x0008, or ... 8\n"
-  "    -04- PVE COMPUTED PROPERTIES    ... ... 0x0010, or .. 16\n"
-  "    -05- PVE ACTIVE LINES   ... ... ... ... 0x0020, or .. 32\n"
-  "    -06- PVE CELLS SEGMENTS ... ... ... ... 0x0040, or .. 64\n"
-  "    -07- PVE SORTED CELLS SEGMENTS  ... ... 0x0080, or . 128\n"
-  "    -08- PVE CELLS  ... ... ... ... ... ... 0x0100, or . 256\n"
-  "    -09- PVE CELLS STACK    ... ... ... ... 0x0200, or . 512\n"
-  "    -10- PVE LINES SEGMENTS ... ... ... ... 0x0400, or  1024\n"
-  "    -11- PVE SORTED LINES SEGMENTS  ... ... 0x0800, or  2048\n"
-  "    -12- PVE LINES  ... ... ... ... ... ... 0x1000, or  4096\n"
-  "    -13- PVE LINES STACK    ... ... ... ... 0x2000, or  8192\n"
-  "    -14- PVE ROOT GAME POSITION ... ... ... 0x4000, or 16384\n"
-  "\n"
-  "Author:\n"
-  "   Written by Roberto Corradini <rob_corradini@yahoo.it>\n"
-  "\n"
-  "Copyright (c) 2015, 2017 Roberto Corradini. All rights reserved.\n"
-  "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n"
-  "This is free software: you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law.\n"
-  ;
-
-
-
-/* Static variables. */
-
-static mop_options_t options;
-
-static int h_flag = false;
-
-static int f_flag = false;
-static char *f_arg = NULL;
-
-static int i_flag = false;
-static char *i_arg = NULL;
-
-static int s_flag = false;
-static int p_flag = false;
-static int t_flag = false;
-static int c_flag = false;
-
-static mop_options_long_t olist[] = {
+static const mop_options_long_t olist[] = {
   {"help",              'h', MOP_NONE},
   {"input-file",        'f', MOP_REQUIRED},
   {"internals",         'i', MOP_REQUIRED},
@@ -164,23 +109,28 @@ static const char *documentation =
   "This is free software: you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law.\n"
   ;
 
-static gchar    *input_file        = NULL;
-static uint64_t  internals         = 0;
-static gboolean  print_summary     = FALSE;
-static gboolean  print_pv          = FALSE;
-static gboolean  print_pv_as_table = FALSE;
-static gboolean  check_invariant   = FALSE;
 
-static const GOptionEntry entries[] =
-  {
-    { "input-file",        'f', 0, G_OPTION_ARG_FILENAME, &input_file,        "Input file name     - Mandatory", NULL },
-    { "internals",         'i', 0, G_OPTION_ARG_INT64,    &internals,         "Print PVE internals - Switches in hex form", NULL },
-    { "print-summary",     's', 0, G_OPTION_ARG_NONE,     &print_summary,     "Print summary       - Reads only the file header and exits", NULL },
-    { "print-pv",          'p', 0, G_OPTION_ARG_NONE,     &print_pv,          "Print pv            - Prints human readable PV", NULL },
-    { "print-pv-as-table", 't', 0, G_OPTION_ARG_NONE,     &print_pv_as_table, "Print pv as table   - Prints PV as a csv file ready for an SQL loader", NULL },
-    { "check-invariant",   'c', 0, G_OPTION_ARG_NONE,     &check_invariant,   "Check PVE invariant - Stops execution if a violation is detected", NULL },
-    { NULL }
-  };
+
+/* Static variables. */
+
+static mop_options_t options;
+
+static int h_flag = false;
+
+static int f_flag = false;
+static char *f_arg = NULL;
+
+static int i_flag = false;
+static char *i_arg = NULL;
+
+static int s_flag = false;
+static int p_flag = false;
+static int t_flag = false;
+static int c_flag = false;
+
+
+
+/* Static functions. */
 
 static bool
 file_exists (const char *const file_name)
@@ -274,59 +224,34 @@ main (int argc, char *argv[])
     return 0;
   }
 
-  /* GLib command line options and argument parsing. */
-  GError *error = NULL;
-  GOptionGroup *option_group = g_option_group_new("name", "description", "help_description", NULL, NULL);
-  GOptionContext *context = g_option_context_new("- Loads a PVE dump file");
-  g_option_context_add_main_entries(context, entries, NULL);
-  g_option_context_add_group(context, option_group);
-  g_option_context_set_description(context, program_documentation_string);
-  if (!g_option_context_parse (context, &argc, &argv, &error)) {
-    g_print("Option parsing failed: %s\n", error->message);
-    return -1;
-  }
-
-  /* Checks command line options for consistency. */
-  if (!input_file) {
-    g_print("Option -f, --file is mandatory.\n");
-    return -2;
-  }
-  if (print_summary && (internals || check_invariant)) {
-    g_print("Option -s, --print-summary, is not compatible with options -i, or -c.\n");
-    return -3;
-  }
-
-  /* Prints summary when the specific option is on. */
-  if (print_summary) {
-    pve_summary_from_binary_file_to_stream(input_file, stdout);
-    return 0;
-  }
-
   /* Loads the full PVE data structure. */
-  PVEnv *pve = pve_load_from_binary_file(input_file);
+  PVEnv *pve = pve_load_from_binary_file(f_arg);
 
   /* Runs a complete check on PVE invariant. */
-  if (check_invariant) {
+  if (c_flag) {
     pve_error_code_t error_code = PVE_ERROR_CODE_OK;
     if (!pve_is_invariant_satisfied(pve, &error_code, 0xFFFFFFFFFFFFFFFF)) {
       printf("Running function pve_is_invariant_satisfied an error has been detected. Error code is: %d\n", error_code);
       free(pve);
-      return -4;
+      return -6;
     }
   }
 
   /* Prints selected internals depending on the specific option switches. */
-  if (internals) {
-    pve_internals_to_stream(pve, stdout, internals);
+  if (i_flag) {
+    // transform i_arg to int
+    // use ... long int strtol(const char *str, char **endptr, int base)
+    // pve_internals_to_stream(pve, stdout, internals);
+    printf("To be implemented!!!\n");
     return 0;
   }
 
-  if (print_pv) {
+  if (p_flag) {
     pve_transform_to_standard_form(pve);
     pve_line_with_variants_to_stream(pve, stdout);
   }
 
-  if (print_pv_as_table) {
+  if (t_flag) {
     board_module_init();
     pve_root_line_as_table_to_stream(pve, stdout);
   }
