643a158f76f16be4293a8e229b764180c40842d6
rcrr <rob_corradini@yahoo.it>
2024-04-09 19:36:53 +0100

    Small changes

--- c/run_regab_solver.sh
+++ c/run_regab_solver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for i in {0..31};
+for i in {0..47};
 do
     ii=$(printf "%02d\n" $i);
-    nohup ./build/bin/regab -a solve -c cfg/regab_gve.cfg -e production -b 9 -y 24 -k gve -n 2000000 > ./build/out/regab.${ii}.log 2>&1 &
+    nohup nice -n 10 ./build/bin/regab -a solve -c cfg/regab_gve.cfg -e production -b 12 -y 25 -k gve -n 1000000 > ./build/out/regab.${ii}.log 2>&1 &
 done
